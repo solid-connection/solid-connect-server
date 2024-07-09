0b6ae568a21ed0bc8b796bfb758726d578afb387
nayonsoso <panda0329@naver.com>
2024-07-10 00:17:05 +0900

    test: 회원가입 시 발생할 수 있는 예외 케이스 추가

--- src/test/java/com/example/solidconnection/e2e/SignUpTest.java
+++ src/test/java/com/example/solidconnection/e2e/SignUpTest.java
@@ -4,21 +4,20 @@ import com.example.solidconnection.auth.dto.SignUpRequest;
 import com.example.solidconnection.auth.dto.SignUpResponse;
 import com.example.solidconnection.config.token.TokenService;
 import com.example.solidconnection.config.token.TokenType;
-import com.example.solidconnection.custom.response.DataResponse;
+import com.example.solidconnection.custom.response.ErrorResponse;
 import com.example.solidconnection.entity.Country;
 import com.example.solidconnection.entity.InterestedCountry;
 import com.example.solidconnection.entity.InterestedRegion;
 import com.example.solidconnection.entity.Region;
-import com.example.solidconnection.entity.SiteUser;
 import com.example.solidconnection.repositories.CountryRepository;
 import com.example.solidconnection.repositories.InterestedCountyRepository;
 import com.example.solidconnection.repositories.InterestedRegionRepository;
 import com.example.solidconnection.repositories.RegionRepository;
+import com.example.solidconnection.siteuser.domain.SiteUser;
 import com.example.solidconnection.siteuser.repository.SiteUserRepository;
 import com.example.solidconnection.type.Gender;
 import com.example.solidconnection.type.PreparationStatus;
 import io.restassured.RestAssured;
-import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,6 +27,11 @@ import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
+import static com.example.solidconnection.custom.exception.ErrorCode.JWT_EXCEPTION;
+import static com.example.solidconnection.custom.exception.ErrorCode.NICKNAME_ALREADY_EXISTED;
+import static com.example.solidconnection.custom.exception.ErrorCode.USER_ALREADY_EXISTED;
+import static com.example.solidconnection.e2e.Fixture.createSiteUserFixtureByEmail;
+import static com.example.solidconnection.e2e.Fixture.createSiteUserFixtureByNickName;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -57,32 +61,29 @@ class SignUpTest extends BaseEndToEndTest {
 
     @Test
     void 유효한_카카오_토큰으로_회원가입한다() {
-        // 테스트를 위한 데이터 셋업
+        // setup - 국가, 지역 정보 저장
         Region region = regionRepository.save(new Region("EROUPE", "유럽"));
         List<Country> countries = countryRepository.saveAll(List.of(
                 new Country("FR", "프랑스", region),
                 new Country("DE", "독일", region)));
 
-        // 카카오 토큰 발급
+        // setup - 카카오 토큰 발급
         String email = "email@email.com";
         String generatedKakaoToken = tokenService.generateToken(email, TokenType.KAKAO_OAUTH);
         tokenService.saveToken(generatedKakaoToken, TokenType.KAKAO_OAUTH);
 
-        // request body 생성
+        // request - body 생성 및 요청
         List<String> interestedRegionNames = List.of("유럽");
         List<String> interestedCountryNames = List.of("프랑스", "독일");
         SignUpRequest signUpRequest = new SignUpRequest(generatedKakaoToken, interestedRegionNames, interestedCountryNames,
                 PreparationStatus.CONSIDERING, "nickname", "profile", Gender.FEMALE, "2000-01-01");
-
         SignUpResponse response = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(signUpRequest)
                 .when().post("/auth/sign-up")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
-                .extract().as(new TypeRef<DataResponse<SignUpResponse>>() {
-                })
-                .data();
+                .extract().as(SignUpResponse.class);
 
         SiteUser savedSiteUser = siteUserRepository.getByEmail(email);
         assertAll(
@@ -107,8 +108,78 @@ class SignUpTest extends BaseEndToEndTest {
                 () -> assertThat(interestedCountries).containsExactlyElementsOf(countries)
         );
 
-        assertThat(redisTemplate.opsForValue().get(TokenType.REFRESH.createTokenKey(email)))
+        assertThat(redisTemplate.opsForValue().get(TokenType.REFRESH.addTokenPrefixToSubject(email)))
                 .as("리프레시 토큰을 저장한다.")
                 .isEqualTo(response.refreshToken());
+    }
+
+    @Test
+    void 이미_있는_닉네임으로_회원가입하면_예외를_응답한다() {
+        // setup - 회원 정보 저장
+        String alreadyExistNickname = "nickname";
+        SiteUser alreadyExistUser = createSiteUserFixtureByNickName(alreadyExistNickname);
+        siteUserRepository.save(alreadyExistUser);
+
+        // setup - 카카오 토큰 발급
+        String email = "email@email.com";
+        String generatedKakaoToken = tokenService.generateToken(email, TokenType.KAKAO_OAUTH);
+        tokenService.saveToken(generatedKakaoToken, TokenType.KAKAO_OAUTH);
+
+        // request - body 생성 및 요청
+        SignUpRequest signUpRequest = new SignUpRequest(generatedKakaoToken, null, null,
+                PreparationStatus.CONSIDERING, alreadyExistNickname, "profile", Gender.FEMALE, "2000-01-01");
+        ErrorResponse errorResponse = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(signUpRequest)
+                .when().post("/auth/sign-up")
+                .then().log().all()
+                .statusCode(HttpStatus.CONFLICT.value())
+                .extract().as(ErrorResponse.class);
+
+        assertThat(errorResponse.message())
+                .isEqualTo(NICKNAME_ALREADY_EXISTED.getMessage());
+    }
+
+    @Test
+    void 이미_있는_이메일로_회원가입하면_예외를_응답한다() {
+        // setup - 회원 정보 저장
+        String alreadyExistEmail = "email@email.com";
+        SiteUser alreadyExistUser = createSiteUserFixtureByEmail(alreadyExistEmail);
+        siteUserRepository.save(alreadyExistUser);
+
+        // setup - 카카오 토큰 발급
+        String generatedKakaoToken = tokenService.generateToken(alreadyExistEmail, TokenType.KAKAO_OAUTH);
+        tokenService.saveToken(generatedKakaoToken, TokenType.KAKAO_OAUTH);
+
+        // request - body 생성 및 요청
+        SignUpRequest signUpRequest = new SignUpRequest(generatedKakaoToken, null, null,
+                PreparationStatus.CONSIDERING, "nickname0", "profile", Gender.FEMALE, "2000-01-01");
+        ErrorResponse errorResponse = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(signUpRequest)
+                .when().post("/auth/sign-up")
+                .then().log().all()
+                .statusCode(HttpStatus.CONFLICT.value())
+                .extract().as(ErrorResponse.class);
+
+        assertThat(errorResponse.message())
+                .isEqualTo(USER_ALREADY_EXISTED.getMessage());
+    }
+
+    @Test
+    void 유효하지_않은_카카오_토큰으로_회원가입을_하면_예외를_응답한다() {
+        SignUpRequest signUpRequest = new SignUpRequest("invalid", null, null,
+                PreparationStatus.CONSIDERING, "nickname", "profile", Gender.FEMALE, "2000-01-01");
+
+        ErrorResponse errorResponse = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(signUpRequest)
+                .when().post("/auth/sign-up")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .extract().as(ErrorResponse.class);
+
+        assertThat(errorResponse.message())
+                .contains(JWT_EXCEPTION.getMessage());
     }
 }
