5703acea811c31a89961f0d1404232962154dbba
sewon <dltpdnjs2000@gmail.com>
2024-08-13 11:24:24 +0900

    feat: 댓글 관련 DTO 정의

---
+++ src/main/java/com/example/solidconnection/comment/dto/CommentCreateRequest.java
@@ -0,0 +1,30 @@
+package com.example.solidconnection.comment.dto;
+
+import com.example.solidconnection.comment.domain.Comment;
+import com.example.solidconnection.post.domain.Post;
+import com.example.solidconnection.siteuser.domain.SiteUser;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Optional;
+
+public record CommentCreateRequest(
+        @NotBlank(message = "댓글 내용은 빈 값일 수 없습니다.")
+        @Size(min = 1, max = 255, message = "댓글 내용은 최소 1자 이상, 최대 255자 이하여야 합니다.")
+        String content,
+        Optional<Long> parentId // https://www.baeldung.com/java-record-optional-param
+) {
+    public Comment toEntity(SiteUser siteUser, Post post, Comment parentComment) {
+
+        Comment comment = new Comment(
+                this.content
+        );
+
+        if (parentComment == null) {
+            comment.setPostAndSiteUser(post, siteUser);
+        } else {
+            comment.setParentCommentAndPostAndSiteUser(parentComment, post, siteUser);
+        }
+        return comment;
+    }
+}
