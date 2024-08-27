b8a1c699c60ef34231298be24c3876570740f4a2
sewon <dltpdnjs2000@gmail.com>
2024-08-27 18:03:14 +0900

    test: 초기 프로필 이미지 수정 테스트코드 추가

--- src/test/java/com/example/solidconnection/unit/service/SiteUserServiceTest.java
+++ src/test/java/com/example/solidconnection/unit/service/SiteUserServiceTest.java
@@ -49,6 +49,7 @@ public class SiteUserServiceTest {
     private SiteUser siteUser;
     private MultipartFile imageFile;
     private UploadedFileUrlResponse uploadedFileUrlResponse;
+    private final String defaultProfileImageUrl = "http://k.kakaocdn.net/dn/o2c5A/btsASaNh2Lr/Xum5kRyuErD8LIuLQEWfC0/img_640x640.jpg";
 
     @BeforeEach
     void setUp() {
@@ -78,6 +79,23 @@ public class SiteUserServiceTest {
 
     private UploadedFileUrlResponse createUploadedFileUrlResponse() {
         return new UploadedFileUrlResponse("https://s3.example.com/test1.png");
+    }
+
+    @Test
+    void 초기_프로필_이미지를_수정한다() {
+        siteUser.setProfileImageUrl(defaultProfileImageUrl);
+        when(siteUserRepository.getByEmail(siteUser.getEmail())).thenReturn(siteUser);
+        when(s3Service.uploadFile(imageFile, ImgType.PROFILE)).thenReturn(uploadedFileUrlResponse);
+
+        // When
+        ProfileImageUpdateResponse profileImageUpdateResponse =
+                siteUserService.updateProfileImage(siteUser.getEmail(), imageFile);
+        // Then
+        assertEquals(profileImageUpdateResponse, ProfileImageUpdateResponse.from(siteUser));
+        verify(siteUserRepository, times(1)).getByEmail(siteUser.getEmail());
+        verify(s3Service, times(0)).deleteExProfile(siteUser.getEmail());
+        verify(s3Service, times(1)).uploadFile(imageFile, ImgType.PROFILE);
+        verify(siteUserRepository, times(1)).save(any(SiteUser.class));
     }
 
     @Test
