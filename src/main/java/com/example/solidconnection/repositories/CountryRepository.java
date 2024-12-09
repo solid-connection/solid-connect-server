7b558a62cb2fc7acff0a2c1403521e2e77d3fb77
nayonsoso <panda0329@naver.com>
2024-12-09 23:57:26 +0900

    style: 사용하지 않는 코드 삭제

--- src/main/java/com/example/solidconnection/repositories/CountryRepository.java
+++ src/main/java/com/example/solidconnection/repositories/CountryRepository.java
@@ -7,16 +7,10 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
 
-    Optional<Country> findByKoreanName(String koreanName);
-
     @Query("SELECT c FROM Country c WHERE c.koreanName IN :names")
     List<Country> findByKoreanNames(@Param(value = "names") List<String> names);
-
-    @Query("SELECT c FROM Country c WHERE c.koreanName LIKE %:keyword%")
-    List<Country> findByKoreanNameContaining(@Param("keyword") String keyword);
 }
--- src/main/java/com/example/solidconnection/repositories/RegionRepository.java
+++ src/main/java/com/example/solidconnection/repositories/RegionRepository.java
@@ -7,12 +7,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
-
-    Optional<Region> findByKoreanName(String koreanName);
 
     @Query("SELECT r FROM Region r WHERE r.koreanName IN :names")
     List<Region> findByKoreanNames(@Param(value = "names") List<String> names);
