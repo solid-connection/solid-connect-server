c26bb177ea566d1dfb5ca4d6b08145e5bb4718aa
Wibaek Park <devmuromi@gmail.com>
2024-12-14 13:23:29 +0900

    chore: cross-origin 허용 url 전환

--- src/main/java/com/example/solidconnection/config/cors/WebConfig.java
+++ src/main/java/com/example/solidconnection/config/cors/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.solid-connect.net")
+                // TODO: 환경변수 분리가 필요해 보임
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.solid-connection.com")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
