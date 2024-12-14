411dde8945232056d0ea8ceb77171baeaf8942c4
Wibaek Park <devmuromi@gmail.com>
2024-12-14 23:15:36 +0900

    feat: CorsPropertiesConfig 추가, cors allowed origins 정보를 yml로 분리한다

--- src/main/java/com/example/solidconnection/config/cors/WebConfig.java
+++ src/main/java/com/example/solidconnection/config/cors/WebConfig.java
@@ -1,17 +1,21 @@
 package com.example.solidconnection.config.cors;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final CorsPropertiesConfig corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 // TODO: 환경변수 분리가 필요해 보임
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.solid-connection.com")
+                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
