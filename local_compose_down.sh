898c18e674c838aa940fa3f2de5b15d4cb73b205
nayonsoso <panda0329@naver.com>
2024-12-22 00:08:14 +0900

    refactor: docker-compose 를 docker compose 로

--- local_compose_down.sh
+++ local_compose_down.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo "Stopping all docker containers..."
-docker-compose -f docker-compose.local.yml down
+docker compose -f docker-compose.local.yml down
 
 echo "Pruning unused Docker images..."
 docker image prune -f
 
 echo "Containers are down and not running."
-docker-compose ps -a
+docker compose ps -a
