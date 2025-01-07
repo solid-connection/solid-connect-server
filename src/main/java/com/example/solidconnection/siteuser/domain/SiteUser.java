bce7fc51751c2aac072e62ae5fd9dcea063b6222
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-01-07 18:05:13 +0900

    refactor: SiteUser entity 내 email에 유니크 키 제약 조건 추가

--- src/main/java/com/example/solidconnection/siteuser/domain/SiteUser.java
+++ src/main/java/com/example/solidconnection/siteuser/domain/SiteUser.java
@@ -38,7 +38,7 @@ public class SiteUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, unique = true)
     private String email;
 
     @Setter
