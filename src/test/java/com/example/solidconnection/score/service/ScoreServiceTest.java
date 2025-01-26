54871c9b9bfa83af0a0ec8f0ab7194fc15a830a4
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-01-27 00:25:07 +0900

    test: 어학 시험 점수 등록 관련 통합테스트 코드 추가

--- src/test/java/com/example/solidconnection/score/service/ScoreServiceTest.java
+++ src/test/java/com/example/solidconnection/score/service/ScoreServiceTest.java
@@ -1,14 +1,19 @@
 package com.example.solidconnection.score.service;
 
 import com.example.solidconnection.score.domain.GpaScore;
+import com.example.solidconnection.score.domain.LanguageTestScore;
 import com.example.solidconnection.score.dto.GpaScoreRequest;
+import com.example.solidconnection.score.dto.LanguageTestScoreRequest;
 import com.example.solidconnection.score.repository.GpaScoreRepository;
+import com.example.solidconnection.score.repository.LanguageTestScoreRepository;
 import com.example.solidconnection.siteuser.domain.SiteUser;
 import com.example.solidconnection.siteuser.repository.SiteUserRepository;
 import com.example.solidconnection.support.integration.BaseIntegrationTest;
 import com.example.solidconnection.type.Gender;
+import com.example.solidconnection.type.LanguageTestType;
 import com.example.solidconnection.type.PreparationStatus;
 import com.example.solidconnection.type.Role;
+import com.example.solidconnection.type.VerifyStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +35,9 @@ class ScoreServiceTest extends BaseIntegrationTest {
     @Autowired
     private SiteUserRepository siteUserRepository;
 
+    @Autowired
+    private LanguageTestScoreRepository languageTestScoreRepository;
+
     @Test
     void GPA_점수를_등록한다() {
         // given
@@ -46,6 +54,26 @@ class ScoreServiceTest extends BaseIntegrationTest {
                 () -> assertThat(savedScore.getGpa().getGpa()).isEqualTo(request.gpa()),
                 () -> assertThat(savedScore.getGpa().getGpaCriteria()).isEqualTo(request.gpaCriteria()),
                 () -> assertThat(savedScore.getIssueDate()).isEqualTo(request.issueDate())
+        );
+    }
+
+    @Test
+    void 어학_시험_점수를_등록한다() {
+        // given
+        SiteUser testUser = createSiteUser();
+        LanguageTestScoreRequest request = createLanguageTestScoreRequest();
+
+        // when
+        Long scoreId = scoreService.submitLanguageTestScore(testUser.getEmail(), request);
+        LanguageTestScore savedScore = languageTestScoreRepository.findById(scoreId).orElseThrow();
+
+        // then
+        assertAll(
+                () -> assertThat(savedScore.getId()).isEqualTo(scoreId),
+                () -> assertThat(savedScore.getLanguageTest().getLanguageTestType()).isEqualTo(request.languageTestType()),
+                () -> assertThat(savedScore.getLanguageTest().getLanguageTestScore()).isEqualTo(request.languageTestScore()),
+                () -> assertThat(savedScore.getIssueDate()).isEqualTo(request.issueDate()),
+                () -> assertThat(savedScore.getVerifyStatus()).isEqualTo(VerifyStatus.PENDING)
         );
     }
 
@@ -66,6 +94,15 @@ class ScoreServiceTest extends BaseIntegrationTest {
         return new GpaScoreRequest(
                 3.5,
                 4.5,
+                LocalDate.now(),
+                "https://example.com/gpa-report.pdf"
+        );
+    }
+
+    private LanguageTestScoreRequest createLanguageTestScoreRequest() {
+        return new LanguageTestScoreRequest(
+                LanguageTestType.TOEFL_IBT,
+                "100",
                 LocalDate.now(),
                 "https://example.com/gpa-report.pdf"
         );
