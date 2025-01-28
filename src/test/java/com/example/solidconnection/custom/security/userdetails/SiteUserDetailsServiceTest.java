9f1ed60dcc13e68400b837b845cc7a0fa573c078
nayonsoso <panda0329@naver.com>
2025-01-29 06:25:49 +0900

    test: 탈퇴한 사용자 검증 테스트 작성

--- src/test/java/com/example/solidconnection/custom/security/userdetails/SiteUserDetailsServiceTest.java
+++ src/test/java/com/example/solidconnection/custom/security/userdetails/SiteUserDetailsServiceTest.java
@@ -8,8 +8,11 @@ import com.example.solidconnection.type.Gender;
 import com.example.solidconnection.type.PreparationStatus;
 import com.example.solidconnection.type.Role;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
 
 import static com.example.solidconnection.custom.exception.ErrorCode.AUTHENTICATION_FAILED;
 import static com.example.solidconnection.custom.exception.ErrorCode.INVALID_TOKEN;
@@ -31,7 +34,7 @@ class SiteUserDetailsServiceTest {
     void 사용자_인증_정보를_반환한다() {
         // given
         SiteUser siteUser = siteUserRepository.save(createSiteUser());
-        String username = siteUser.getId().toString();
+        String username = getUserName(siteUser);
 
         // when
         SiteUserDetails userDetails = (SiteUserDetails) userDetailsService.loadUserByUsername(username);
@@ -39,31 +42,48 @@ class SiteUserDetailsServiceTest {
         // then
         assertAll(
                 () -> assertThat(userDetails.getUsername()).isEqualTo(username),
-                () -> assertThat(userDetails.getSiteUser()).isNotNull(),
                 () -> assertThat(userDetails.getSiteUser()).extracting("id").isEqualTo(siteUser.getId())
         );
     }
 
-    @Test
-    void 지정되지_않은_형식의_식별자가_주어지면_예외_응답을_반환한다() {
-        // given
-        String username = "notNumber";
+    @Nested
+    class 예외_응답을_반환한다 {
 
-        // when & then
-        assertThatCode(() -> userDetailsService.loadUserByUsername(username))
-                .isInstanceOf(CustomException.class)
-                .hasMessageContaining(INVALID_TOKEN.getMessage());
-    }
+        @Test
+        void 지정되지_않은_형식의_식별자가_주어지면_예외_응답을_반환한다() {
+            // given
+            String username = "notNumber";
 
-    @Test
-    void 식별자에_해당하는_사용자가_없으면_예외_응답을_반환한다() {
-        // given
-        String username = "1234";
+            // when & then
+            assertThatCode(() -> userDetailsService.loadUserByUsername(username))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessageContaining(INVALID_TOKEN.getMessage());
+        }
 
-        // when & then
-        assertThatCode(() -> userDetailsService.loadUserByUsername(username))
-                .isInstanceOf(CustomException.class)
-                .hasMessageContaining(AUTHENTICATION_FAILED.getMessage());
+        @Test
+        void 식별자에_해당하는_사용자가_없으면_예외_응답을_반환한다() {
+            // given
+            String username = "1234";
+
+            // when & then
+            assertThatCode(() -> userDetailsService.loadUserByUsername(username))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessageContaining(AUTHENTICATION_FAILED.getMessage());
+        }
+
+        @Test
+        void 탈퇴한_사용자이면_예외_응답을_반환한다() {
+            // given
+            SiteUser siteUser = createSiteUser();
+            siteUser.setQuitedAt(LocalDate.now());
+            siteUserRepository.save(siteUser);
+            String username = getUserName(siteUser);
+
+            // when & then
+            assertThatCode(() -> userDetailsService.loadUserByUsername(username))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessageContaining(AUTHENTICATION_FAILED.getMessage());
+        }
     }
 
     private SiteUser createSiteUser() {
@@ -76,5 +96,9 @@ class SiteUserDetailsServiceTest {
                 Role.MENTEE,
                 Gender.MALE
         );
+    }
+
+    private String getUserName(SiteUser siteUser) {
+        return siteUser.getId().toString();
     }
 }
