36575a35cb52b4992cc9b2711fd7c563e6171c64
nayonsoso <panda0329@naver.com>
2025-02-08 15:47:32 +0900

    refactor: 이메일 형식으로 검증하노록 dto 어노테이션 수정

--- src/main/java/com/example/solidconnection/auth/dto/EmailSignUpTokenRequest.java
+++ src/main/java/com/example/solidconnection/auth/dto/EmailSignUpTokenRequest.java
@@ -1,10 +1,11 @@
 package com.example.solidconnection.auth.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record EmailSignUpTokenRequest(
 
-        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "이메일을 입력해주세요.")
         String email,
 
         @NotBlank(message = "비밀번호를 입력해주세요.")
