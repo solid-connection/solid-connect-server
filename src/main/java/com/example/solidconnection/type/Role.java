c58f732e1d0d8c16fc7caf7ec7883f315aa8bb00
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-02-09 17:45:19 +0900

    feat: Role에 ADMIN 추가 및 Spring Security 권한 처리 추가

--- src/main/java/com/example/solidconnection/type/Role.java
+++ src/main/java/com/example/solidconnection/type/Role.java
@@ -1,6 +1,18 @@
 package com.example.solidconnection.type;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
 public enum Role {
+
+    ADMIN,
     MENTOR,
-    MENTEE
+    MENTEE;
+
+    private static final String ROLE_PREFIX = "ROLE_";
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(ROLE_PREFIX + name()));
+    }
 }
