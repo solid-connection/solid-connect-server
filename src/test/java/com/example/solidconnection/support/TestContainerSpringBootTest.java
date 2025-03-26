23991f9f39229e38522dcfb6192aa3255fb71703
nayonsoso <panda0329@naver.com>
2025-03-26 16:58:02 +0900

    refactor: redis testcontainers 설정 수정

--- src/test/java/com/example/solidconnection/support/TestContainerSpringBootTest.java
+++ src/test/java/com/example/solidconnection/support/TestContainerSpringBootTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.lang.annotation.ElementType;
@@ -12,10 +13,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @ExtendWith({DatabaseClearExtension.class})
+@ContextConfiguration(initializers = RedisTestContainer.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
-@Import({MySQLTestContainer.class, RedisTestContainer.class})
+@Import({MySQLTestContainer.class})
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface TestContainerSpringBootTest {
