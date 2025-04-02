6dfc6ed7ebd3f859fa5f482fc5d073d43eb5e0d4
Yeongseo Na <panda0329@naver.com>
2025-04-02 16:49:52 +0900

    chore: ci 스크립트 작성 및 ci 통과를 위한 코드 수정 (#266)
    * chore: ci 스크립트 작성
    
    * chore: 테스트용 application.yml 분리
    
    * test: test profile 제거
    
    * refactor: 애플 시크릿키를 yml로 이동
    
    * refactor: 함수 이름 변경
    
    * refactor: 오타 수정
    
    * refactor: redis testcontainers 설정 수정
    
    * refactor: ddl-auto 옵션 변경
    
    - create-drop을 create로 변경
    - 테스트 컨테이너를 실행해서 테스트하기 때문에, 테스트가 종료되면 당연히 테이블이 삭제된다. 따라서 create-drop을 하는것과 create를 하는 것이 동일하게 동작한다. 굳이 create로 바꾼 것은 삭제 시 FK 위반으로 WARN 로그가 불필요하게 백몇줄이 찍히는걸 막기 위해서.
    
    * test: e2e 테스트 수정
    
    - 깨지는 테스트 수정
    - 비지니스 로직 변경으로 더 이상 해당되지 않는 테스트 삭제
    
    * chore: createAt과 updateAt의 나노초 길이 고정
    
    * style: 주석 추가
    
    * refactor: MySQL 테스트 컨테이너 설정 변경
    
    - redis 테스트 컨테이너와 동일하게 설정하여 불필요한 인지 부하를 줄인다.

--- src/main/java/com/example/solidconnection/auth/client/AppleOAuthClientSecretProvider.java
+++ src/main/java/com/example/solidconnection/auth/client/AppleOAuthClientSecretProvider.java
@@ -9,20 +9,17 @@ import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Date;
-import java.util.stream.Collectors;
 
 import static com.example.solidconnection.custom.exception.ErrorCode.FAILED_TO_READ_APPLE_PRIVATE_KEY;
 
 /*
- * 애플 OAuth 에 필요하 클라이언트 시크릿은 매번 동적으로 생성해야 한다.
+ * 애플 OAuth 에 필요한 클라이언트 시크릿은 매번 동적으로 생성해야 한다.
  * 클라이언트 시크릿은 애플 개발자 계정에서 발급받은 개인키(*.p8)를 사용하여 JWT 를 생성한다.
  * https://developer.apple.com/documentation/accountorganizationaldatasharing/creating-a-client-secret
  * */
@@ -32,14 +29,13 @@ public class AppleOAuthClientSecretProvider {
 
     private static final String KEY_ID_HEADER = "kid";
     private static final long TOKEN_DURATION = 1000 * 60 * 10; // 10min
-    private static final String SECRET_KEY_PATH = "secret/AppleOAuthKey.p8";
 
     private final AppleOAuthClientProperties appleOAuthClientProperties;
     private PrivateKey privateKey;
 
     @PostConstruct
     private void initPrivateKey() {
-        privateKey = readPrivateKey();
+        privateKey = loadPrivateKey();
     }
 
     public String generateClientSecret() {
@@ -57,16 +53,14 @@ public class AppleOAuthClientSecretProvider {
                 .compact();
     }
 
-    private PrivateKey readPrivateKey() {
-        try (InputStream is = getClass().getClassLoader().getResourceAsStream(SECRET_KEY_PATH);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
-
-            String secretKey = reader.lines().collect(Collectors.joining("\n"));
+    private PrivateKey loadPrivateKey() {
+        try {
+            String secretKey = appleOAuthClientProperties.secretKey();
             byte[] encoded = Base64.decodeBase64(secretKey);
             PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
             KeyFactory keyFactory = KeyFactory.getInstance("EC");
             return keyFactory.generatePrivate(keySpec);
-        } catch (Exception e) {
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             throw new CustomException(FAILED_TO_READ_APPLE_PRIVATE_KEY);
         }
     }
