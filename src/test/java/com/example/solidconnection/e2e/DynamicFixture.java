0cf113d3db20b7436c4b446ef59f04070ffaf350
Yeongseo Na <panda0329@naver.com>
2025-04-09 19:37:23 +0900

    refactor: e2e 테스트 제거 (#278)
    * refactor: e2e 관련 테스트 코드 삭제
    
    * chore: todo 주석 추가
    
    * chore: rest assured 의존성 제거

--- src/test/java/com/example/solidconnection/e2e/DynamicFixture.java
+++ src/test/java/com/example/solidconnection/e2e/DynamicFixture.java
@@ -1,22 +1,10 @@
 package com.example.solidconnection.e2e;
 
-import com.example.solidconnection.application.domain.Gpa;
-import com.example.solidconnection.application.domain.LanguageTest;
-import com.example.solidconnection.auth.dto.oauth.KakaoUserInfoDto;
 import com.example.solidconnection.siteuser.domain.SiteUser;
-import com.example.solidconnection.type.LanguageTestType;
 import com.example.solidconnection.type.PreparationStatus;
 import com.example.solidconnection.type.Role;
-import com.example.solidconnection.type.SemesterAvailableForDispatch;
-import com.example.solidconnection.type.TuitionFeeType;
-import com.example.solidconnection.university.domain.LanguageRequirement;
-import com.example.solidconnection.university.domain.LikedUniversity;
-import com.example.solidconnection.university.domain.University;
-import com.example.solidconnection.university.domain.UniversityInfoForApply;
 
-import java.util.Set;
-
-public class DynamicFixture {
+public class DynamicFixture { // todo: test fixture 개선 작업 이후, 이 클래스의 사용이 대체되면 삭제 필요
 
     public static SiteUser createSiteUserByEmail(String email) {
         return new SiteUser(
@@ -26,62 +14,5 @@ public class DynamicFixture {
                 PreparationStatus.CONSIDERING,
                 Role.MENTEE
         );
-    }
-
-    public static SiteUser createSiteUserByNickName(String nickname) {
-        return new SiteUser(
-                "email@email.com",
-                nickname,
-                "profileImage",
-                PreparationStatus.CONSIDERING,
-                Role.MENTEE
-        );
-    }
-
-    public static KakaoUserInfoDto createKakaoUserInfoDtoByEmail(String email) {
-        return new KakaoUserInfoDto(
-                new KakaoUserInfoDto.KakaoAccountDto(
-                        new KakaoUserInfoDto.KakaoAccountDto.KakaoProfileDto(
-                                "nickname",
-                                "profileImageUrl"
-                        ),
-                        email
-                )
-        );
-    }
-
-    public static UniversityInfoForApply createUniversityForApply(
-            String term, University university, Set<LanguageRequirement> languageRequirements) {
-        return new UniversityInfoForApply(
-                null,
-                term,
-                "koreanName",
-                1,
-                TuitionFeeType.HOME_UNIVERSITY_PAYMENT,
-                SemesterAvailableForDispatch.ONE_SEMESTER,
-                "1",
-                "detailsForLanguage",
-                "gpaRequirement",
-                "gpaRequirementCriteria",
-                "detailsForApply",
-                "detailsForMajor",
-                "detailsForAccommodation",
-                "detailsForEnglishCourse",
-                "details",
-                languageRequirements,
-                university);
-    }
-
-    public static LikedUniversity createLikedUniversity(
-            SiteUser siteUser, UniversityInfoForApply universityInfoForApply) {
-        return new LikedUniversity(null, universityInfoForApply, siteUser);
-    }
-
-    public static Gpa createDummyGpa() {
-        return new Gpa(3.5, 4.0, "gpaReportUrl");
-    }
-
-    public static LanguageTest createDummyLanguageTest() {
-        return new LanguageTest(LanguageTestType.TOEIC, "900", "toeicReportUrl");
     }
 }
