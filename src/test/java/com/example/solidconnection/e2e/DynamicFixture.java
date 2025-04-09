c16420ad0677c1ffff02c030bb45a788aa15c730
nayonsoso <panda0329@naver.com>
2025-04-09 17:25:42 +0900

    chore: todo 주석 추가

--- src/test/java/com/example/solidconnection/e2e/DynamicFixture.java
+++ src/test/java/com/example/solidconnection/e2e/DynamicFixture.java
@@ -5,7 +5,7 @@ import com.example.solidconnection.type.Gender;
 import com.example.solidconnection.type.PreparationStatus;
 import com.example.solidconnection.type.Role;
 
-public class DynamicFixture {
+public class DynamicFixture { // todo: test fixture 개선 작업 이후, 이 클래스의 사용이 대체되면 삭제 필요
 
     public static SiteUser createSiteUserByEmail(String email) {
         return new SiteUser(
