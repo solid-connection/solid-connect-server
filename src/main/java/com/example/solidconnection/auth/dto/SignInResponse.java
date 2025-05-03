af06df832e49040eaad08f293693d1decff6df3d
nayonsoso <panda0329@naver.com>
2025-05-04 02:42:08 +0900

    refactor: signInService에 토큰 객체 적용

--- src/main/java/com/example/solidconnection/auth/dto/SignInResponse.java
+++ src/main/java/com/example/solidconnection/auth/dto/SignInResponse.java
@@ -1,7 +1,14 @@
 package com.example.solidconnection.auth.dto;
 
+import com.example.solidconnection.auth.service.AccessToken;
+import com.example.solidconnection.auth.service.RefreshToken;
+
 public record SignInResponse(
         String accessToken,
         String refreshToken
 ) {
+
+    public static SignInResponse of(AccessToken accessToken, RefreshToken refreshToken) {
+        return new SignInResponse(accessToken.token(), refreshToken.token());
+    }
 }
