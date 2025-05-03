d33933e10015c4fbb3b3b5127e17bc2631a91042
nayonsoso <panda0329@naver.com>
2025-05-04 03:50:36 +0900

    test: AuthService 테스트 보충

--- src/test/java/com/example/solidconnection/auth/service/AuthServiceTest.java
+++ src/test/java/com/example/solidconnection/auth/service/AuthServiceTest.java
@@ -1,16 +1,13 @@
 package com.example.solidconnection.auth.service;
 
-import com.example.solidconnection.auth.domain.TokenType;
 import com.example.solidconnection.auth.dto.ReissueRequest;
 import com.example.solidconnection.auth.dto.ReissueResponse;
-import com.example.solidconnection.config.security.JwtProperties;
 import com.example.solidconnection.custom.exception.CustomException;
 import com.example.solidconnection.siteuser.domain.SiteUser;
 import com.example.solidconnection.siteuser.repository.SiteUserRepository;
 import com.example.solidconnection.support.TestContainerSpringBootTest;
 import com.example.solidconnection.type.PreparationStatus;
 import com.example.solidconnection.type.Role;
-import com.example.solidconnection.util.JwtUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -35,19 +32,16 @@ class AuthServiceTest {
     @Autowired
     private SiteUserRepository siteUserRepository;
 
-    @Autowired
-    private JwtProperties jwtProperties;
-
     @Test
     void 로그아웃한다() {
         // given
-        String accessToken = "accessToken";
+        AccessToken accessToken = new AccessToken("subject", "accessToken");
 
         // when
         authService.signOut(accessToken);
 
         // then
-        assertThat(authTokenProvider.findBlackListToken(accessToken)).isNotNull();
+        assertThat(authTokenProvider.isTokenBlacklisted(accessToken.token())).isTrue();
     }
 
     @Test
@@ -67,26 +61,25 @@ class AuthServiceTest {
     class 토큰을_재발급한다 {
 
         @Test
-        void 요청의_리프레시_토큰이_저장되어_있고_값이_일치면_액세스_토큰을_재발급한다() {
+        void 요청의_리프레시_토큰이_저장되어_있으면_액세스_토큰을_재발급한다() {
             // given
-            SiteUser siteUser = createSiteUser();
-            String refreshToken = authTokenProvider.generateAndSaveRefreshToken(siteUser);
-            ReissueRequest reissueRequest = new ReissueRequest(refreshToken);
+            RefreshToken refreshToken = authTokenProvider.generateAndSaveRefreshToken(new Subject("subject"));
+            ReissueRequest reissueRequest = new ReissueRequest(refreshToken.token());
 
             // when
             ReissueResponse reissuedAccessToken = authService.reissue(reissueRequest);
 
-            // then
-            String actualSubject = JwtUtils.parseSubject(reissuedAccessToken.accessToken(), jwtProperties.secret());
-            String expectedSubject = JwtUtils.parseSubject(refreshToken, jwtProperties.secret());
+            // then - 요청의 리프레시 토큰과 재발급한 액세스 토큰의 subject 가 동일해야 한다.
+            Subject expectedSubject = authTokenProvider.parseSubject(refreshToken.token());
+            Subject actualSubject = authTokenProvider.parseSubject(reissuedAccessToken.accessToken());
             assertThat(actualSubject).isEqualTo(expectedSubject);
         }
 
         @Test
         void 요청의_리프레시_토큰이_저장되어있지_않다면_예외_응답을_반환한다() {
             // given
-            String refreshToken = authTokenProvider.generateToken("subject", TokenType.REFRESH);
-            ReissueRequest reissueRequest = new ReissueRequest(refreshToken);
+            String invalidRefreshToken = authTokenProvider.generateAccessToken(new Subject("subject")).token();
+            ReissueRequest reissueRequest = new ReissueRequest(invalidRefreshToken);
 
             // when, then
             assertThatCode(() -> authService.reissue(reissueRequest))
