134b2de3f1e34dfc716f520f66f46bf94f23bc0c
Wibaek Park <devmuromi@gmail.com>
2025-05-10 01:10:21 +0900

    chore: import 수정

--- src/main/java/com/example/solidconnection/news/domain/News.java
+++ src/main/java/com/example/solidconnection/news/domain/News.java
@@ -1,7 +1,11 @@
 package com.example.solidconnection.news.domain;
 
 import com.example.solidconnection.entity.common.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
