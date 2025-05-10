602e4942367e99ae717db76f85c927077afb867d
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-05-10 14:35:43 +0900

    feat: RegionFixture에 지역 생성 메서드 추가

--- src/test/java/com/example/solidconnection/region/fixture/RegionFixture.java
+++ src/test/java/com/example/solidconnection/region/fixture/RegionFixture.java
@@ -16,4 +16,18 @@ public class RegionFixture {
                 .koreanName("영미권")
                 .findOrCreate();
     }
+
+    public Region 유럽() {
+        return regionFixtureBuilder.region()
+                .code("EUROPE")
+                .koreanName("유럽")
+                .findOrCreate();
+    }
+
+    public Region 아시아() {
+        return regionFixtureBuilder.region()
+                .code("ASIA")
+                .koreanName("아시아")
+                .findOrCreate();
+    }
 }
