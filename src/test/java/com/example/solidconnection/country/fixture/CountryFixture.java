efa00128333da465b878ec4696ce5108e2ac4826
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-05-10 14:37:33 +0900

    feat: CountryFixture에 국가 생성 메서드 추가

--- src/test/java/com/example/solidconnection/country/fixture/CountryFixture.java
+++ src/test/java/com/example/solidconnection/country/fixture/CountryFixture.java
@@ -19,4 +19,36 @@ public class CountryFixture {
                 .region(regionFixture.영미권())
                 .findOrCreate();
     }
+
+    public Country 캐나다() {
+        return countryFixtureBuilder.country()
+                .code("CA")
+                .koreanName("캐나다")
+                .region(regionFixture.영미권())
+                .findOrCreate();
+    }
+
+    public Country 덴마크() {
+        return countryFixtureBuilder.country()
+                .code("DK")
+                .koreanName("덴마크")
+                .region(regionFixture.유럽())
+                .findOrCreate();
+    }
+
+    public Country 오스트리아() {
+        return countryFixtureBuilder.country()
+                .code("AT")
+                .koreanName("오스트리아")
+                .region(regionFixture.유럽())
+                .findOrCreate();
+    }
+
+    public Country 일본() {
+        return countryFixtureBuilder.country()
+                .code("JP")
+                .koreanName("일본")
+                .region(regionFixture.아시아())
+                .findOrCreate();
+    }
 }
