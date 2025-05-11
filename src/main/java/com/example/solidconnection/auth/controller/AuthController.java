5f1474a53f4dd7a63b72325108b19dd038b926c7
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-05-12 00:01:05 +0900

    refactor: 회원 탈퇴 API HTTP Method를 PATCH에서 DELETE로 변경 (#311)
    * refactor: 회원 탈퇴 API HTTP Method를 PATCH에서 DELETE로 변경
    
    * chore: 불필요한 import 제거

--- src/main/java/com/example/solidconnection/auth/controller/AuthController.java
+++ src/main/java/com/example/solidconnection/auth/controller/AuthController.java
@@ -26,7 +26,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -102,7 +102,7 @@ public class AuthController {
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping("/quit")
+    @DeleteMapping("/quit")
     public ResponseEntity<Void> quit(
             @AuthorizedUser SiteUser siteUser,
             Authentication authentication // todo: #299를 작업하며 인자를 (Authentication authentication)만 받도록 수정해야 함
