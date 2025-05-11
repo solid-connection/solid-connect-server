40b62cf066c02e4c586e92e503161263847d95f6
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-05-11 21:11:57 +0900

    chore: 불필요한 import 제거

--- src/main/java/com/example/solidconnection/auth/controller/AuthController.java
+++ src/main/java/com/example/solidconnection/auth/controller/AuthController.java
@@ -27,7 +27,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
