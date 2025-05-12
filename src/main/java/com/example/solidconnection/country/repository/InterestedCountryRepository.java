cc6efd106e081d6516efb471d9acd5f907c3932d
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-05-12 14:22:22 +0900

    chore: InterestedCountryRepository 오타 수정

--- src/main/java/com/example/solidconnection/country/repository/InterestedCountryRepository.java
+++ src/main/java/com/example/solidconnection/country/repository/InterestedCountryRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface InterestedCountyRepository extends JpaRepository<InterestedCountry, Long> {
+public interface InterestedCountryRepository extends JpaRepository<InterestedCountry, Long> {
     List<InterestedCountry> findAllBySiteUser(SiteUser siteUser);
 }
