96e9c623c6805c8951849d5ce36677cc8abe4f2d
Yeonri <lsy1307@inha.edu>
2025-05-26 22:50:27 +0900

    refactor: Flyway V13 script 등록 - Application FK제약조건 제거 - column명 변경 - Index 추가

---
+++ src/main/resources/db/migration/V13_add_application_index_and_delete_manny_to_one_mapping.sql
@@ -0,0 +1,19 @@
+ALTER TABLE application DROP FOREIGN KEY FKeajojvwgn069mfxhbq5ja1sws;
+ALTER TABLE application DROP FOREIGN KEY FKepp2by7frnkt1o1w3v4t4lgtu;
+ALTER TABLE application DROP FOREIGN KEY FKi822ljuirbu9o0lnd9jt7l7qg;
+
+ALTER TABLE application RENAME COLUMN first_choice_university_id TO first_choice_university_apply_info_id;
+ALTER TABLE application RENAME COLUMN second_choice_university_id TO second_choice_university_apply_info_id;
+ALTER TABLE application RENAME COLUMN third_choice_university_id TO third_choice_university_apply_info_id;
+
+CREATE INDEX idx_app_user_term_delete
+    ON application(site_user_id, term, is_delete);
+
+CREATE INDEX idx_app_first_choice_search
+    ON application(verify_status, term, is_delete, first_choice_university_apply_info_id);
+
+CREATE INDEX idx_app_second_choice_search
+    ON application(verify_status, term, is_delete, second_choice_university_apply_info_id);
+
+CREATE INDEX idx_app_third_choice_search
+    ON application(verify_status, term, is_delete, third_choice_university_apply_info_id);
