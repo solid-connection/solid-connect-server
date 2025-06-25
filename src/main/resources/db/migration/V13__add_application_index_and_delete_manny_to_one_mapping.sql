22037f841af0cf63b50c6fe71b42fb2a02f38f9f
nayonsoso <panda0329@naver.com>
2025-06-25 11:30:20 +0900

    chore: flyway 스크립트 컬럼 오타 수정

--- src/main/resources/db/migration/V13__add_application_index_and_delete_manny_to_one_mapping.sql
+++ src/main/resources/db/migration/V13__add_application_index_and_delete_manny_to_one_mapping.sql
@@ -6,10 +6,10 @@ CREATE INDEX idx_app_user_term_delete
     ON application(site_user_id, term, is_delete);
 
 CREATE INDEX idx_app_first_choice_search
-    ON application(verify_status, term, is_delete, first_choice_university_apply_info_id);
+    ON application(verify_status, term, is_delete, first_choice_university_info_for_apply_id);
 
 CREATE INDEX idx_app_second_choice_search
-    ON application(verify_status, term, is_delete, second_choice_university_apply_info_id);
+    ON application(verify_status, term, is_delete, second_choice_university_info_for_apply_id);
 
 CREATE INDEX idx_app_third_choice_search
-    ON application(verify_status, term, is_delete, third_choice_university_apply_info_id);
+    ON application(verify_status, term, is_delete, third_choice_university_info_for_apply_id);
