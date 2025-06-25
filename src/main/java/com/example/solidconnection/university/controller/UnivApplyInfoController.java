bd3e1f394f64cda5ac5377c1b3770392339d7119
nayonsoso <panda0329@naver.com>
2025-06-25 14:19:32 +0900

    refactor: UnivApplyInfo 사용하도록 컨트롤러 함수 변경

--- src/main/java/com/example/solidconnection/university/controller/UnivApplyInfoController.java
+++ src/main/java/com/example/solidconnection/university/controller/UnivApplyInfoController.java
@@ -27,7 +27,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/univ-apply-infos")
 @RestController
-public class UniversityController {
+public class UnivApplyInfoController {
 
     private final UniversityQueryService universityQueryService;
     private final UniversityLikeService universityLikeService;
@@ -35,7 +35,7 @@ public class UniversityController {
     private final MyPageService myPageService;
 
     @GetMapping("/recommend")
-    public ResponseEntity<UniversityRecommendsResponse> getUniversityRecommends(
+    public ResponseEntity<UniversityRecommendsResponse> getUnivApplyInfoRecommends(
             @AuthorizedUser(required = false) SiteUser siteUser
     ) {
         if (siteUser == null) {
@@ -46,7 +46,7 @@ public class UniversityController {
     }
 
     @GetMapping("/like")
-    public ResponseEntity<List<UniversityInfoForApplyPreviewResponse>> getMyWishUniversity( // todo: wish 가 아니라 liked 로 변경 필요 - 코드 용어 통일
+    public ResponseEntity<List<UniversityInfoForApplyPreviewResponse>> getMyWishUnivApplyInfo( /* todo: wish 가 아니라 liked 로 변경 필요 - 코드 용어 통일 */
             @AuthorizedUser SiteUser siteUser
     ) {
         List<UniversityInfoForApplyPreviewResponse> wishUniversities = myPageService.getWishUniversity(siteUser);
@@ -63,7 +63,7 @@ public class UniversityController {
     }
 
     @PostMapping("/{univ-apply-info-id}/like")
-    public ResponseEntity<LikeResultResponse> addWishUniversity(
+    public ResponseEntity<LikeResultResponse> addWishUnivApplyInfo(
             @AuthorizedUser SiteUser siteUser,
             @PathVariable("univ-apply-info-id") Long univApplyInfoId
     ) {
@@ -72,7 +72,7 @@ public class UniversityController {
     }
 
     @DeleteMapping("/{univ-apply-info-id}/like")
-    public ResponseEntity<LikeResultResponse> cancelWishUniversity(
+    public ResponseEntity<LikeResultResponse> cancelWishUnivApplyInfo(
             @AuthorizedUser SiteUser siteUser,
             @PathVariable("univ-apply-info-id") Long univApplyInfoId
     ) {
@@ -81,16 +81,16 @@ public class UniversityController {
     }
 
     @GetMapping("/{univ-apply-info-id}")
-    public ResponseEntity<UniversityDetailResponse> getUniversityDetails(
-            @PathVariable("univ-apply-info-id") Long universityInfoForApplyId
+    public ResponseEntity<UniversityDetailResponse> getUnivApplyInfoDetails(
+            @PathVariable("univ-apply-info-id") Long univApplyInfoId
     ) {
-        UniversityDetailResponse universityDetailResponse = universityQueryService.getUniversityDetail(universityInfoForApplyId);
+        UniversityDetailResponse universityDetailResponse = universityQueryService.getUniversityDetail(univApplyInfoId);
         return ResponseEntity.ok(universityDetailResponse);
     }
 
     // todo return타입 UniversityInfoForApplyPreviewResponses로 추후 수정 필요
     @GetMapping("/search")
-    public ResponseEntity<List<UniversityInfoForApplyPreviewResponse>> searchUniversity(
+    public ResponseEntity<List<UniversityInfoForApplyPreviewResponse>> searchUnivApplyInfo(
             @RequestParam(required = false, defaultValue = "") String region,
             @RequestParam(required = false, defaultValue = "") List<String> keyword,
             @RequestParam(required = false, defaultValue = "") LanguageTestType testType,
