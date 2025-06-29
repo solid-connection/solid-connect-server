e002fccd43dc11f40730ea03e3d7e44c9ac93803
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-06-29 13:59:55 +0900

    test: 소식지 생성 테스트 추가 - 소식지 관련 fixture 추가

---
+++ src/test/java/com/example/solidconnection/news/fixture/NewsFixture.java
@@ -0,0 +1,22 @@
+package com.example.solidconnection.news.fixture;
+
+import com.example.solidconnection.news.domain.News;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.test.context.TestComponent;
+
+@TestComponent
+@RequiredArgsConstructor
+public class NewsFixture {
+
+    private final NewsFixtureBuilder newsFixtureBuilder;
+
+    public News 소식지(long siteUserId) {
+        return newsFixtureBuilder
+                .title("소식지 제목")
+                .description("소식지 설명")
+                .thumbnailUrl("news/5a02ba2f-38f5-4ae9-9a24-53d624a18233")
+                .url("https://youtu.be/test")
+                .siteUserId(siteUserId)
+                .create();
+    }
+}
