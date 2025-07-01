bacdbbb731d1aa6bfd5111c87de206966ce14bf0
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-07-02 00:44:46 +0900

    refactor: @Repository 어노테이션 안붙이도록 통일 (#360)

--- src/main/java/com/example/solidconnection/community/post/repository/PostImageRepository.java
+++ src/main/java/com/example/solidconnection/community/post/repository/PostImageRepository.java
@@ -2,8 +2,6 @@ package com.example.solidconnection.community.post.repository;
 
 import com.example.solidconnection.community.post.domain.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
 }
--- src/main/java/com/example/solidconnection/siteuser/repository/SiteUserRepository.java
+++ src/main/java/com/example/solidconnection/siteuser/repository/SiteUserRepository.java
@@ -1,18 +1,15 @@
 package com.example.solidconnection.siteuser.repository;
 
-
 import com.example.solidconnection.siteuser.domain.AuthType;
 import com.example.solidconnection.siteuser.domain.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
 
     Optional<SiteUser> findByEmailAndAuthType(String email, AuthType authType);
