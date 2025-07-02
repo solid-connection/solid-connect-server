3f987087354616b40c3d0deb55753fc704442a1e
seonghyeok <chosh2001@naver.com>
2025-07-02 13:19:22 +0900

    test: 멘토 관련 픽스처 생성

---
+++ src/test/java/com/example/solidconnection/mentor/fixture/MentoringFixtureBuilder.java
@@ -0,0 +1,77 @@
+package com.example.solidconnection.mentor.fixture;
+
+import com.example.solidconnection.common.VerifyStatus;
+import com.example.solidconnection.mentor.domain.Mentoring;
+import com.example.solidconnection.mentor.repository.MentoringRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.test.context.TestComponent;
+
+import java.time.ZonedDateTime;
+
+@TestComponent
+@RequiredArgsConstructor
+public class MentoringFixtureBuilder {
+
+    private final MentoringRepository mentoringRepository;
+
+    private ZonedDateTime createdAt;
+    private ZonedDateTime confirmedAt;
+    private ZonedDateTime checkedAt;
+    private VerifyStatus verifyStatus = VerifyStatus.PENDING;
+    private String rejectedReason;
+    private long mentorId;
+    private long menteeId;
+
+    public MentoringFixtureBuilder mentoring() {
+        return new MentoringFixtureBuilder(mentoringRepository);
+    }
+
+    public MentoringFixtureBuilder createdAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public MentoringFixtureBuilder confirmedAt(ZonedDateTime confirmedAt) {
+        this.confirmedAt = confirmedAt;
+        return this;
+    }
+
+    public MentoringFixtureBuilder checkedAt(ZonedDateTime checkedAt) {
+        this.checkedAt = checkedAt;
+        return this;
+    }
+
+    public MentoringFixtureBuilder verifyStatus(VerifyStatus verifyStatus) {
+        this.verifyStatus = verifyStatus;
+        return this;
+    }
+
+    public MentoringFixtureBuilder rejectedReason(String rejectedReason) {
+        this.rejectedReason = rejectedReason;
+        return this;
+    }
+
+    public MentoringFixtureBuilder mentorId(long mentorId) {
+        this.mentorId = mentorId;
+        return this;
+    }
+
+    public MentoringFixtureBuilder menteeId(long menteeId) {
+        this.menteeId = menteeId;
+        return this;
+    }
+
+    public Mentoring create() {
+        Mentoring mentoring = new Mentoring(
+                null,
+                createdAt,
+                confirmedAt,
+                checkedAt,
+                verifyStatus,
+                rejectedReason,
+                mentorId,
+                menteeId
+        );
+        return mentoringRepository.save(mentoring);
+    }
+}
