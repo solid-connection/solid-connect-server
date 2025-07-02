4717e7adf179de7fabe8010bfd3902a01775b410
nayonsoso <panda0329@naver.com>
2025-07-03 03:14:26 +0900

    feat: 멘토 미리보기 Batch 조회를 위한 레포지토리 생성
    - Mentor와 SiteUser는 id 만 참조하는 관계이다.
    - Mentor와 Mentoring도 id 만 참조하는 관계이다.
    - "멘토 목록"에 대해서 매번 siteUser, mentoring과 join 하면 N+1 이 발생한다.
    - 이를 해결하기 위해, 한번에 조회하고 매핑하여 1번의 쿼리로 해결한다.

--- src/main/java/com/example/solidconnection/mentor/repository/MentoringRepository.java
+++ src/main/java/com/example/solidconnection/mentor/repository/MentoringRepository.java
@@ -2,8 +2,18 @@ package com.example.solidconnection.mentor.repository;
 
 import com.example.solidconnection.mentor.domain.Mentoring;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MentoringRepository extends JpaRepository<Mentoring, Long> {
 
     boolean existsByMentorIdAndMenteeId(long mentorId, long menteeId);
+
+    @Query("""
+            SELECT m FROM Mentoring m
+            WHERE m.mentorId IN :mentorIds AND m.menteeId = :menteeId
+            """)
+    List<Mentoring> findAllByMentorIdInAndMenteeId(@Param("mentorIds") List<Long> mentorIds, @Param("menteeId") long menteeId);
 }
