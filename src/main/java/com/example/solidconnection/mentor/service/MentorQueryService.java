7c8419b0f116dee755b31a16e7b814fbc8e1f674
nayonsoso <panda0329@naver.com>
2025-07-03 03:16:31 +0900

    feat: 멘토 미리보기 목록 서비스 함수 생성

--- src/main/java/com/example/solidconnection/mentor/service/MentorQueryService.java
+++ src/main/java/com/example/solidconnection/mentor/service/MentorQueryService.java
@@ -3,13 +3,22 @@ package com.example.solidconnection.mentor.service;
 import com.example.solidconnection.common.exception.CustomException;
 import com.example.solidconnection.mentor.domain.Mentor;
 import com.example.solidconnection.mentor.dto.MentorDetailResponse;
+import com.example.solidconnection.mentor.dto.MentorPreviewResponse;
+import com.example.solidconnection.mentor.dto.MentorPreviewsResponse;
+import com.example.solidconnection.mentor.repository.MentorBatchQueryRepository;
 import com.example.solidconnection.mentor.repository.MentorRepository;
 import com.example.solidconnection.mentor.repository.MentoringRepository;
 import com.example.solidconnection.siteuser.domain.SiteUser;
 import com.example.solidconnection.siteuser.repository.SiteUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static com.example.solidconnection.common.exception.ErrorCode.MENTOR_NOT_FOUND;
 
@@ -17,9 +26,12 @@ import static com.example.solidconnection.common.exception.ErrorCode.MENTOR_NOT_
 @Service
 public class MentorQueryService {
 
+    private static final int NO_NEXT_PAGE_NUMBER = -1;
+
     private final MentorRepository mentorRepository;
     private final MentoringRepository mentoringRepository;
     private final SiteUserRepository siteUserRepository;
+    private final MentorBatchQueryRepository mentorBatchQueryRepository;
 
     @Transactional(readOnly = true)
     public MentorDetailResponse getMentorDetails(long mentorId, SiteUser currentUser) {
@@ -30,5 +42,34 @@ public class MentorQueryService {
         boolean isApplied = mentoringRepository.existsByMentorIdAndMenteeId(mentorId, currentUser.getId());
 
         return MentorDetailResponse.of(mentor, mentorUser, isApplied);
+    }
+
+    @Transactional(readOnly = true)
+    public MentorPreviewsResponse getMentorPreviews(String region, SiteUser siteUser, Pageable pageable) { // todo: 멘토의 '인증' 작업 후 region 필터링 추가
+        Page<Mentor> mentorPage = mentorRepository.findAll(pageable);
+        List<Mentor> mentors = mentorPage.toList();
+
+        List<MentorPreviewResponse> content = getContent(mentors, siteUser);
+        int pageNumber = getPageNumber(mentorPage);
+
+        return new MentorPreviewsResponse(content, pageNumber);
+    }
+
+    private List<MentorPreviewResponse> getContent(List<Mentor> mentors, SiteUser siteUser) {
+        Map<Long, SiteUser> mentorIdToSiteUser = mentorBatchQueryRepository.getMentorIdToSiteUserMap(mentors);
+        Map<Long, Boolean> mentorIdToIsApplied = mentorBatchQueryRepository.getMentorIdToIsApplied(mentors, siteUser.getId());
+
+        List<MentorPreviewResponse> mentorPreviews = new ArrayList<>();
+        for (Mentor mentor : mentors) {
+            SiteUser mentorUser = mentorIdToSiteUser.get(mentor.getId());
+            boolean isApplied = mentorIdToIsApplied.get(mentor.getId());
+            MentorPreviewResponse response = MentorPreviewResponse.of(mentor, mentorUser, isApplied);
+            mentorPreviews.add(response);
+        }
+        return mentorPreviews;
+    }
+
+    private int getPageNumber(Page<Mentor> page) {
+        return page.hasNext() ? page.nextPageable().getPageNumber() + 1 : NO_NEXT_PAGE_NUMBER; // one-based index
     }
 }
