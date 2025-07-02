fe7aefc911099c29e3166f1f31798380a45436ce
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-07-03 00:07:52 +0900

    feat: 소식지 좋아요 엔티티 추가 (#359)
    * chore: flyway 스크립트 추가
    
    * feat: 소식지 좋아요 엔티티 추가

---
+++ src/main/java/com/example/solidconnection/news/domain/LikedNews.java
@@ -0,0 +1,36 @@
+package com.example.solidconnection.news.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uk_liked_news_site_user_id_news_id",
+                columnNames = {"site_user_id", "news_id"}
+        )
+})
+public class LikedNews {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "news_id")
+    private long newsId;
+
+    @Column(name = "site_user_id")
+    private long siteUserId;
+}
