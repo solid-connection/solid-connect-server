3d83350377ee6834d916bf6cea081e92dea2540d
nayonsoso <panda0329@naver.com>
2025-07-03 03:30:54 +0900

    feat: 멘토 미리보기 목록 컨트롤러 생성

--- src/main/java/com/example/solidconnection/mentor/controller/MentorController.java
+++ src/main/java/com/example/solidconnection/mentor/controller/MentorController.java
@@ -2,14 +2,20 @@ package com.example.solidconnection.mentor.controller;
 
 import com.example.solidconnection.common.resolver.AuthorizedUser;
 import com.example.solidconnection.mentor.dto.MentorDetailResponse;
+import com.example.solidconnection.mentor.dto.MentorPreviewsResponse;
 import com.example.solidconnection.mentor.service.MentorQueryService;
 import com.example.solidconnection.siteuser.domain.SiteUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RequiredArgsConstructor
 @RequestMapping("/mentors")
@@ -19,11 +25,21 @@ public class MentorController {
     private final MentorQueryService mentorQueryService;
 
     @GetMapping("/{mentor-id}")
-    public ResponseEntity<?> getMentorDetails(
+    public ResponseEntity<MentorDetailResponse> getMentorDetails(
             @AuthorizedUser SiteUser siteUser,
             @PathVariable("mentor-id") Long mentorId
     ) {
         MentorDetailResponse response = mentorQueryService.getMentorDetails(mentorId, siteUser);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<MentorPreviewsResponse> getMentorPreviews(
+            @AuthorizedUser SiteUser siteUser,
+            @RequestParam("region") String region,
+            @PageableDefault(size = 3, sort = "menteeCount", direction = DESC) Pageable pageable
+    ) {
+        MentorPreviewsResponse response = mentorQueryService.getMentorPreviews(region, siteUser, pageable);
         return ResponseEntity.ok(response);
     }
 }
