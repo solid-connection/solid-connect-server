c5c3761cde0f3172dba3f2ab8ba11984ed78f30d
nayonsoso <panda0329@naver.com>
2025-07-03 15:53:07 +0900

    test: 멘토 미리보기 목록 조회 테스트 코드 작성

--- src/test/java/com/example/solidconnection/mentor/service/MentorQueryServiceTest.java
+++ src/test/java/com/example/solidconnection/mentor/service/MentorQueryServiceTest.java
@@ -6,16 +6,24 @@ import com.example.solidconnection.mentor.domain.Channel;
 import com.example.solidconnection.mentor.domain.Mentor;
 import com.example.solidconnection.mentor.dto.ChannelResponse;
 import com.example.solidconnection.mentor.dto.MentorDetailResponse;
+import com.example.solidconnection.mentor.dto.MentorPreviewResponse;
+import com.example.solidconnection.mentor.dto.MentorPreviewsResponse;
 import com.example.solidconnection.mentor.fixture.ChannelFixture;
 import com.example.solidconnection.mentor.fixture.MentorFixture;
 import com.example.solidconnection.mentor.fixture.MentoringFixture;
 import com.example.solidconnection.siteuser.domain.SiteUser;
 import com.example.solidconnection.siteuser.fixture.SiteUserFixture;
 import com.example.solidconnection.support.TestContainerSpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -41,6 +49,7 @@ class MentorQueryServiceTest {
     private ChannelFixture channelFixture;
 
     private long universityId = 1L; // todo: 멘토 인증 기능 추가 변경 필요
+    private String region = "아시아";
 
     @Nested
     class 멘토_단일_조회_성공 {
@@ -101,6 +110,84 @@ class MentorQueryServiceTest {
             assertThatCode(() -> mentorQueryService.getMentorDetails(notExistingMentorId, siteUserFixture.사용자()))
                     .isInstanceOf(CustomException.class)
                     .hasMessageContaining(ErrorCode.MENTOR_NOT_FOUND.getMessage());
+        }
+    }
+
+    @Nested
+    class 멘토_미리보기_목록_조회 {
+
+        private static final int NO_NEXT_PAGE_NUMBER = -1;
+
+        private Mentor mentor1, mentor2;
+        private SiteUser mentorUser1, mentorUser2, currentUser;
+
+        @BeforeEach
+        void setUp() {
+            currentUser = siteUserFixture.사용자(1, "사용자1");
+            mentorUser1 = siteUserFixture.사용자(2, "멘토1");
+            mentorUser2 = siteUserFixture.사용자(3, "멘토2");
+            mentor1 = mentorFixture.멘토(mentorUser1.getId(), universityId);
+            mentor2 = mentorFixture.멘토(mentorUser2.getId(), universityId);
+        }
+
+        @Test
+        void 멘토_미리보기_목록의_정보를_조회한다() {
+            // given
+            Channel channel1 = channelFixture.채널(1, mentor1);
+            Channel channel2 = channelFixture.채널(2, mentor2);
+
+            // when
+            MentorPreviewsResponse response = mentorQueryService.getMentorPreviews(region, currentUser, PageRequest.of(0, 10));
+
+            // then
+            Map<Long, MentorPreviewResponse> mentorPreviewMap = response.content().stream()
+                    .collect(Collectors.toMap(MentorPreviewResponse::id, Function.identity()));
+
+            assertAll(
+                    () -> assertThat(mentorPreviewMap.get(mentor1.getId())).extracting(MentorPreviewResponse::nickname)
+                            .isEqualTo(mentorUser1.getNickname()),
+                    () -> assertThat(mentorPreviewMap.get(mentor1.getId()).channels()).extracting(ChannelResponse::url)
+                            .containsOnly(channel1.getUrl()),
+                    () -> assertThat(mentorPreviewMap.get(mentor2.getId())).extracting(MentorPreviewResponse::nickname)
+                            .isEqualTo(mentorUser2.getNickname()),
+                    () -> assertThat(mentorPreviewMap.get(mentor2.getId()).channels()).extracting(ChannelResponse::url)
+                            .containsOnly(channel2.getUrl())
+            );
+        }
+
+        @Test
+        void 멘토들에_대한_나의_멘토링_지원_여부를_조회한다() {
+            // given
+            mentoringFixture.대기중_멘토링(mentor1.getId(), currentUser.getId());
+
+            // when
+            MentorPreviewsResponse response = mentorQueryService.getMentorPreviews(region, currentUser, PageRequest.of(0, 10));
+
+            // then
+            Map<Long, MentorPreviewResponse> mentorPreviewMap = response.content().stream()
+                    .collect(Collectors.toMap(MentorPreviewResponse::id, Function.identity()));
+            assertAll(
+                    () -> assertThat(mentorPreviewMap.get(mentor1.getId()).isApplied()).isTrue(),
+                    () -> assertThat(mentorPreviewMap.get(mentor2.getId()).isApplied()).isFalse()
+            );
+        }
+
+        @Test
+        void 다음_페이지_번호를_응답한다() {
+            // given
+            MentorPreviewsResponse response = mentorQueryService.getMentorPreviews(region, currentUser, PageRequest.of(0, 1));
+
+            // then
+            assertThat(response.nextPageNumber()).isEqualTo(2);
+        }
+
+        @Test
+        void 다음_페이지가_없으면_페이지_없음을_의미하는_값을_응답한다() {
+            // given
+            MentorPreviewsResponse response = mentorQueryService.getMentorPreviews(region, currentUser, PageRequest.of(0, 10));
+
+            // then
+            assertThat(response.nextPageNumber()).isEqualTo(NO_NEXT_PAGE_NUMBER);
         }
     }
 }
