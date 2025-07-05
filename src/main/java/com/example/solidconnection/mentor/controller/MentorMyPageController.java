ddfb3154e19a6e0d6a3fc2decea7a5a91bc7445d
nayonsoso <panda0329@naver.com>
2025-07-06 02:29:42 +0900

    feat: 멘토 마이페이지 수정 컨트롤러 구현

--- src/main/java/com/example/solidconnection/mentor/controller/MentorMyPageController.java
+++ src/main/java/com/example/solidconnection/mentor/controller/MentorMyPageController.java
@@ -1,16 +1,21 @@
 package com.example.solidconnection.mentor.controller;
 
 import com.example.solidconnection.common.resolver.AuthorizedUser;
+import com.example.solidconnection.mentor.dto.MentorMyPageUpdateRequest;
 import com.example.solidconnection.mentor.dto.MentorMyPageResponse;
 import com.example.solidconnection.mentor.service.MentorMyPageService;
 import com.example.solidconnection.security.annotation.RequireRoleAccess;
 import com.example.solidconnection.siteuser.domain.Role;
 import com.example.solidconnection.siteuser.domain.SiteUser;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RequestMapping("/mentor/my")
@@ -26,5 +31,16 @@ public class MentorMyPageController {
     ) {
         MentorMyPageResponse mentorMyPageResponse = mentorMyPageService.getMentorMyPage(siteUser);
         return ResponseEntity.ok(mentorMyPageResponse);
+    }
+
+    @RequireRoleAccess(roles = {Role.MENTOR})
+    @PutMapping
+    public ResponseEntity<String> updateMentorMyPage(
+            @AuthorizedUser SiteUser siteUser,
+            @Valid MentorMyPageUpdateRequest mentorMyPageUpdateRequest,
+            @RequestParam(value = "file", required = false) MultipartFile imageFile
+    ) {
+        mentorMyPageService.updateMentorMyPage(siteUser, mentorMyPageUpdateRequest, imageFile);
+        return ResponseEntity.ok().build();
     }
 }
