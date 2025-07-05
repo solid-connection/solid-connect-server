d0714354bd75ad6b948da0d6f75c1faca84a13a1
nayonsoso <panda0329@naver.com>
2025-07-06 00:25:59 +0900

    feat: 멘토 마이페이지 응답 dto 생성

---
+++ src/main/java/com/example/solidconnection/mentor/dto/MentorMyPageResponse.java
@@ -0,0 +1,38 @@
+package com.example.solidconnection.mentor.dto;
+
+import com.example.solidconnection.mentor.domain.Mentor;
+import com.example.solidconnection.siteuser.domain.ExchangeStatus;
+import com.example.solidconnection.siteuser.domain.SiteUser;
+
+import java.util.List;
+
+public record MentorMyPageResponse(
+        long id,
+        String profileImageUrl,
+        String nickname,
+        ExchangeStatus exchangeStatus,
+        String country,
+        String universityName,
+        int menteeCount,
+        boolean hasBadge,
+        String introduction,
+        List<ChannelResponse> channels
+) {
+
+    public static MentorMyPageResponse of(Mentor mentor, SiteUser siteUser) {
+        return new MentorMyPageResponse(
+                mentor.getId(),
+                siteUser.getProfileImageUrl(),
+                siteUser.getNickname(),
+                siteUser.getExchangeStatus(),
+                "국가", // todo: 교환학생 기록이 인증되면 추가
+                "대학 이름",
+                mentor.getMenteeCount(),
+                mentor.isHasBadge(),
+                mentor.getIntroduction(),
+                mentor.getChannels().stream()
+                        .map(ChannelResponse::from)
+                        .toList()
+        );
+    }
+}
