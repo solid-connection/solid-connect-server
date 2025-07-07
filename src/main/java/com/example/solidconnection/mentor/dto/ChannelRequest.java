f84b6ababc4fca1e7211bffd6ecc9827dc2f37fb
nayonsoso <panda0329@naver.com>
2025-07-07 23:50:33 +0900

    refactor: 누락한 검증 메세지 추가

--- src/main/java/com/example/solidconnection/mentor/dto/ChannelRequest.java
+++ src/main/java/com/example/solidconnection/mentor/dto/ChannelRequest.java
@@ -6,10 +6,10 @@ import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.URL;
 
 public record ChannelRequest(
-        @NotNull
+        @NotNull(message = "채널 종류를 입력해주세요.")
         ChannelType type,
 
-        @NotBlank
+        @NotBlank(message = "채널 URL을 입력해주세요.")
         @URL
         String url
 ) {
