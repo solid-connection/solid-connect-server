a627f6b2ee26bda4fe1b3e003b210fd09f9fcfbf
nayonsoso <panda0329@naver.com>
2025-07-18 23:02:06 +0900

    chore: 경로와 관련된 변수 분리

--- docs/script/local_compose_up.sh
+++ docs/script/local_compose_up.sh
@@ -3,18 +3,20 @@
 # 명령이 0이 아닌 종료값을 가질때 즉시 종료
 set -e
 
-if [ ! -d "mysql_data_local" ]; then
-  echo "mysql_data_local 디렉토리가 없습니다. 디렉토리를 생성합니다."
-  mkdir -p ../../mysql_data_local
+BASE_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+
+if [ ! -d "${BASE_DIR}/mysql_data_local" ]; then
+  echo "mysql_data_local 디렉토리가 없습니다. ${BASE_DIR}/mysql_data_local 디렉토리를 생성합니다."
+  mkdir -p "${BASE_DIR}/mysql_data_local"
 fi
 
-if [ ! -d "redis_data_local" ]; then
-  echo "redis_data_local 디렉토리가 없습니다. 디렉토리를 생성합니다."
-  mkdir -p ../../redis_data_local
+if [ ! -d "${BASE_DIR}/redis_data_local" ]; then
+  echo "redis_data_local 디렉토리가 없습니다. ${BASE_DIR}/redis_data_local 디렉토리를 생성합니다."
+  mkdir -p "${BASE_DIR}/redis_data_local"
 fi
 
 echo "Starting all docker containers..."
-docker compose -f ../../docker-compose.local.yml up -d
+docker compose -f "${BASE_DIR}/docker-compose.local.yml" up -d
 
 echo "Pruning unused Docker images..."
 docker image prune -f
