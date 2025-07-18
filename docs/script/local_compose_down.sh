69365db16b76eb5064bb57233b70fb00a20b1272
Yeongseo Na <panda0329@naver.com>
2025-07-18 23:08:15 +0900

    chore: 문서, 스크립트 파일들 폴더 변경 (#390)
    * chore: infra 관련 설정 파일 위치 이동
    
    * chore: 스크립트 파일 위치 이동
    
    * refactor: 위치 변경에 따른 상대 경로 수정
    
    * chore: 경로와 관련된 변수 분리

--- docs/script/local_compose_down.sh
+++ docs/script/local_compose_down.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+BASE_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+
 echo "Stopping all docker containers..."
-docker compose -f docker-compose.local.yml down
+docker compose -f "${BASE_DIR}/docker-compose.local.yml" down
 
 echo "Pruning unused Docker images..."
 docker image prune -f
