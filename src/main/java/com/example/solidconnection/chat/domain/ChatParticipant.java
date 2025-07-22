7d7608f7e87ff57ca8689b500189793c177a486d
seonghyeok <chosh2001@naver.com>
2025-07-22 14:53:12 +0900

    feat: chat_participant 테이블에서 chat_room, site_user 간 고유 키 생성

--- src/main/java/com/example/solidconnection/chat/domain/ChatParticipant.java
+++ src/main/java/com/example/solidconnection/chat/domain/ChatParticipant.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,12 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uk_chat_participant_chat_room_site_user",
+                columnNames = {"chat_room_id", "site_user_id"}
+        )
+})
 public class ChatParticipant extends BaseEntity {
 
     @Id
