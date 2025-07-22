b4f76e4d85c46f7c72cc2691032b5fc878945f97
seonghyeok <chosh2001@naver.com>
2025-07-22 14:54:23 +0900

    chore: ChatReadStatus 엔티티에 UK 제약조건 추가

--- src/main/java/com/example/solidconnection/chat/domain/ChatReadStatus.java
+++ src/main/java/com/example/solidconnection/chat/domain/ChatReadStatus.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,12 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uk_chat_read_status_chat_room_participant",
+                columnNames = {"chat_room_id", "chat_participant_id"}
+        )
+})
 public class ChatReadStatus extends BaseEntity {
 
     @Id
