bc271c3cb2fc7a9a1a7da0e50b15c24027b77edf
nayonsoso <panda0329@naver.com>
2025-07-23 11:34:57 +0900

    refactor: 거절 사유 입력 기능 제거

--- src/main/java/com/example/solidconnection/mentor/dto/MentoringConfirmRequest.java
+++ src/main/java/com/example/solidconnection/mentor/dto/MentoringConfirmRequest.java
@@ -5,9 +5,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record MentoringConfirmRequest(
         @NotNull(message = "승인 상태를 설정해주세요.")
-        VerifyStatus status,
-
-        String rejectedReason
+        VerifyStatus status
 ) {
 
 }
