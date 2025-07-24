5aeec2845311cab06e465de1eaa1bf71dd6925fd
nayonsoso <panda0329@naver.com>
2025-07-25 00:32:04 +0900

    feat: 리프레시 토큰을 쿠키로 설정하는 클래스 생성

---
+++ src/main/java/com/example/solidconnection/auth/controller/RefreshTokenCookieManager.java
@@ -0,0 +1,40 @@
+package com.example.solidconnection.auth.controller;
+
+import com.example.solidconnection.auth.domain.TokenType;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RefreshTokenCookieManager {
+
+    private static final String COOKIE_NAME = "refreshToken";
+    private static final String PATH = "/";
+    private static final String SAME_SITE = "Strict";
+
+    public void setCookie(HttpServletResponse response, String refreshToken) {
+        ResponseCookie cookie = ResponseCookie.from(COOKIE_NAME, refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .path(PATH)
+                .maxAge(changeMicroSecondToSecond(TokenType.REFRESH.getExpireTime()))  // 초단위
+                .sameSite(SAME_SITE)
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+
+    private long changeMicroSecondToSecond(long microSeconds) {
+        return microSeconds / 1000;
+    }
+
+    public void deleteCookie(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from(COOKIE_NAME, "")
+                .httpOnly(true)
+                .secure(true)
+                .path(PATH)
+                .maxAge(0) // 쿠키 삭제를 위해 maxAge를 0으로 설정
+                .sameSite(SAME_SITE)
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+}
