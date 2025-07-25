94580183b2d7c6934a8048d54866ab4479628632
seonghyeok cho <65901319+whqtker@users.noreply.github.com>
2025-07-25 19:19:52 +0900

    feat: 채팅 관련 엔티티 및 DDL 작성 (#395)
    * feat: 채팅 관련 엔티티 생성
    
    * feat: 채팅 관련 테이블의 DDL 작성
    
    * fix: DDL 콤마 누락 문제 해결
    
    * feat: chat_participant 테이블에서 chat_room, site_user 간 고유 키 생성
    
    * chore: ChatReadStatus 엔티티에 UK 제약조건 추가
    
    * fix: JPA가 UK 제약 조건 생성 시 컬럼을 찾지 못하는 문제 해결
    
    - 명시적으로 컬럼 이름을 지정한다.
    
    * chore: UK 제약 조건 이름 변경
    
    * chore: isGroup, hasAttachment 필드에 기본값 생성
    
    - 데이터 타입을 primitive type으로 지정
    - Column 어노테이션 제거 (App level과 DB level 간 일관성 보장)
    - DDL에 기본값 설정
    
    * chore: hasAttachment 컬럼 제거
    
    * chore: flyway 파일 버전 수정

---
+++ src/main/resources/db/migration/V24__add_chat_related_tables.sql
@@ -0,0 +1,55 @@
+CREATE TABLE chat_room
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    is_group BOOLEAN NOT NULL DEFAULT false,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL
+);
+
+CREATE TABLE chat_participant
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    site_user_id BIGINT NOT NULL,
+    chat_room_id BIGINT NOT NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    CONSTRAINT FK_CHAT_PARTICIPANT_CHAT_ROOM_ID FOREIGN KEY (chat_room_id) REFERENCES chat_room (id),
+    CONSTRAINT FK_CHAT_PARTICIPANT_SITE_USER_ID FOREIGN KEY (site_user_id) REFERENCES site_user (id),
+    CONSTRAINT UK_CHAT_PARTICIPANT_CHAT_ROOM_ID_SITE_USER_ID UNIQUE (chat_room_id, site_user_id)
+);
+
+CREATE TABLE chat_message
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(500) NOT NULL,
+    sender_id BIGINT NOT NULL,
+    chat_room_id BIGINT NOT NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    CONSTRAINT FK_CHAT_MESSAGE_CHAT_ROOM_ID FOREIGN KEY (chat_room_id) REFERENCES chat_room (id),
+    CONSTRAINT FK_CHAT_MESSAGE_SENDER_ID FOREIGN KEY (sender_id) REFERENCES chat_participant (id)
+);
+
+CREATE TABLE chat_attachment
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    is_image BOOLEAN NOT NULL,
+    url VARCHAR(500) NOT NULL,
+    thumbnail_url VARCHAR(500),
+    chat_message_id BIGINT NOT NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    CONSTRAINT FK_CHAT_ATTACHMENT_CHAT_MESSAGE_ID FOREIGN KEY (chat_message_id) REFERENCES chat_message (id)
+);
+
+CREATE TABLE chat_read_status
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    chat_room_id BIGINT NOT NULL,
+    chat_participant_id BIGINT NOT NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    CONSTRAINT FK_CHAT_READ_STATUS_CHAT_ROOM_ID FOREIGN KEY (chat_room_id) REFERENCES chat_room (id),
+    CONSTRAINT FK_CHAT_READ_STATUS_CHAT_PARTICIPANT_ID FOREIGN KEY (chat_participant_id) REFERENCES chat_participant (id),
+    CONSTRAINT UK_CHAT_READ_STATUS_CHAT_ROOM_ID_CHAT_PARTICIPANT_ID UNIQUE (chat_room_id, chat_participant_id)
+);
