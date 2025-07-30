d8710a0ee0db6eced04869b39e297d35f06b6979
Yeongseo Na <panda0329@naver.com>
2025-07-30 11:42:09 +0900

    refactor: 멘토 조회 시, 파견 상태 대신 '파견 학기'를 응답하도록 (#412)
    * refactor: 멘토 테이블에 '파견 학기' 컬럼을 추가
    
    * refactor: 멘토 조회 시, 파견 상태 대신 '파견 학기'를 응답하도록
    
    - 멘터 상세 페이지, 미리보기, 멘토 마이페이지에서 변경이 일어남
    
    * refactor: 사용하지 않는 클래스 삭제
    
    - sliceResponse 로 대체됨

--- src/main/java/com/example/solidconnection/mentor/dto/MentorDetailResponse.java
+++ src/main/java/com/example/solidconnection/mentor/dto/MentorDetailResponse.java
@@ -1,7 +1,6 @@
 package com.example.solidconnection.mentor.dto;
 
 import com.example.solidconnection.mentor.domain.Mentor;
-import com.example.solidconnection.siteuser.domain.ExchangeStatus;
 import com.example.solidconnection.siteuser.domain.SiteUser;
 import java.util.List;
 
@@ -9,9 +8,9 @@ public record MentorDetailResponse(
         long id,
         String nickname,
         String profileImageUrl,
-        ExchangeStatus exchangeStatus,
         String country,
         String universityName,
+        String term,
         int menteeCount,
         boolean hasBadge,
         String introduction,
@@ -25,9 +24,9 @@ public record MentorDetailResponse(
                 mentor.getId(),
                 mentorUser.getNickname(),
                 mentorUser.getProfileImageUrl(),
-                mentorUser.getExchangeStatus(),
                 "국가", // todo: 교환학생 기록이 인증되면 추가
                 "대학 이름",  // todo: 교환학생 기록이 인증되면 추가
+                mentor.getTerm(),
                 mentor.getMenteeCount(),
                 mentor.isHasBadge(),
                 mentor.getIntroduction(),
