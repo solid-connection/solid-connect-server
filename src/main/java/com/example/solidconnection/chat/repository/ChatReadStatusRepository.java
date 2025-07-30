d9ad86b7e29cd81eed6d17fd8cebd6ece9a58889
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-07-31 00:34:42 +0900

    refactor: DB와 영속성 컨텍스트 간 불일치 방지를 위한 옵션 추가

--- src/main/java/com/example/solidconnection/chat/repository/ChatReadStatusRepository.java
+++ src/main/java/com/example/solidconnection/chat/repository/ChatReadStatusRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface ChatReadStatusRepository extends JpaRepository<ChatReadStatus, Long> {
 
-    @Modifying
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query(value = """
                    INSERT INTO chat_read_status (chat_room_id, chat_participant_id, created_at, updated_at)
                    VALUES (:chatRoomId, :chatParticipantId, NOW(6), NOW(6))
