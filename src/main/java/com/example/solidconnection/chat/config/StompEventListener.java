69a0bc90c0f4d9847be953ebc48d0c2d84f0428d
Yeon <84384499+lsy1307@users.noreply.github.com>
2025-07-31 19:25:24 +0900

    feat: STOMP Config 추가 (#400)
    * feat: Stomp Config 추가
    - build.gradle 의존성 추가
    - ErrorCode 추가
    - config 작성
    
    * feat: 수정사항 반영
    - 불필요한 의존성 삭제
    - 불필요한 파일 삭제
    - 매직넘버 application-variable에서 관리
    - 에러코드 추가
    - 중복 코드 함수화
    
    * fix: 테스트 코드 에러 해결을 위한 프로퍼티 기본값 추가
    
    * refactor: 수정사항 반영
    - 로그 삭제
    - @ConfigurationProperties 사용을 위한 파일 추가
    - application.yml에 websocket 값 추가
    
    * refactor: 수정사항 반영
    - application-variable.yml에 설정값 추가
    - code formatting, 오타 수정
    - 불필요한 @Sl4fj제거
    - StompWebSocketConfig 변수 분리
    
    * chore: secret 해시 수정

---
+++ src/main/java/com/example/solidconnection/chat/config/StompEventListener.java
@@ -0,0 +1,27 @@
+package com.example.solidconnection.chat.config;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class StompEventListener {
+
+    private final Set<String> sessions = ConcurrentHashMap.newKeySet();
+
+    @EventListener
+    public void connectHandle(SessionConnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        sessions.add(accessor.getSessionId());
+    }
+
+    @EventListener
+    public void disconnectHandle(SessionDisconnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        sessions.remove(accessor.getSessionId());
+    }
+}
---
+++ src/main/java/com/example/solidconnection/chat/config/StompWebSocketConfig.java
@@ -0,0 +1,55 @@
+package com.example.solidconnection.chat.config;
+
+import com.example.solidconnection.chat.config.StompProperties.HeartbeatProperties;
+import com.example.solidconnection.chat.config.StompProperties.InboundProperties;
+import com.example.solidconnection.chat.config.StompProperties.OutboundProperties;
+import com.example.solidconnection.security.config.CorsProperties;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+    private final StompProperties stompProperties;
+    private final CorsProperties corsProperties;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        List<String> strings = corsProperties.allowedOrigins();
+        String[] allowedOrigins = strings.toArray(String[]::new);
+        registry.addEndpoint("/connect").setAllowedOrigins(allowedOrigins).withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        InboundProperties inboundProperties = stompProperties.threadPool().inbound();
+        registration.interceptors(stompHandler).taskExecutor().corePoolSize(inboundProperties.corePoolSize()).maxPoolSize(inboundProperties.maxPoolSize()).queueCapacity(inboundProperties.queueCapacity());
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        OutboundProperties outboundProperties = stompProperties.threadPool().outbound();
+        registration.taskExecutor().corePoolSize(outboundProperties.corePoolSize()).maxPoolSize(outboundProperties.maxPoolSize());
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-");
+        scheduler.initialize();
+        HeartbeatProperties heartbeatProperties = stompProperties.heartbeat();
+        registry.setApplicationDestinationPrefixes("/publish");
+        registry.enableSimpleBroker("/topic").setHeartbeatValue(new long[]{heartbeatProperties.serverInterval(), heartbeatProperties.clientInterval()}).setTaskScheduler(scheduler);
+    }
+}
