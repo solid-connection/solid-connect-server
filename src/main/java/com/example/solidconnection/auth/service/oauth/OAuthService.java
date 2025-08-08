43c5c220ede1c3914e6cdde50811b1c26307ca49
nayonsoso <panda0329@naver.com>
2025-08-09 05:59:31 +0900

    refactor: 전략패턴 적용

--- src/main/java/com/example/solidconnection/auth/service/oauth/OAuthService.java
+++ src/main/java/com/example/solidconnection/auth/service/oauth/OAuthService.java
@@ -1,6 +1,5 @@
 package com.example.solidconnection.auth.service.oauth;
 
-
 import com.example.solidconnection.auth.dto.SignInResponse;
 import com.example.solidconnection.auth.dto.oauth.OAuthCodeRequest;
 import com.example.solidconnection.auth.dto.oauth.OAuthResponse;
@@ -12,6 +11,8 @@ import com.example.solidconnection.siteuser.domain.AuthType;
 import com.example.solidconnection.siteuser.domain.SiteUser;
 import com.example.solidconnection.siteuser.repository.SiteUserRepository;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /*
@@ -19,43 +20,36 @@ import org.springframework.transaction.annotation.Transactional;
  * 기존 회원 : 로그인한다.
  * 신규 회원 : 회원가입할 때 필요한 정보를 제공한다.
  * */
-public abstract class OAuthService {
+@Service
+@RequiredArgsConstructor
+public class OAuthService {
 
     private final OAuthSignUpTokenProvider OAuthSignUpTokenProvider;
     private final SignInService signInService;
     private final SiteUserRepository siteUserRepository;
-
-    protected OAuthService(OAuthSignUpTokenProvider OAuthSignUpTokenProvider, SiteUserRepository siteUserRepository, SignInService signInService) {
-        this.OAuthSignUpTokenProvider = OAuthSignUpTokenProvider;
-        this.siteUserRepository = siteUserRepository;
-        this.signInService = signInService;
-    }
+    private final OAuthClientMap oauthClientMap;
 
     @Transactional
-    public OAuthResponse processOAuth(OAuthCodeRequest oauthCodeRequest) {
-        OAuthUserInfoDto userInfoDto = getOAuthUserInfo(oauthCodeRequest.code());
-        String email = userInfoDto.getEmail();
-        Optional<SiteUser> optionalSiteUser = siteUserRepository.findByEmailAndAuthType(email, getAuthType());
+    public OAuthResponse processOAuth(AuthType authType, OAuthCodeRequest codeRequest) {
+        OAuthClient oauthClient = oauthClientMap.getOAuthClient(authType);
+        OAuthUserInfoDto userInfo = oauthClient.getUserInfo(codeRequest.code());
+        Optional<SiteUser> optionalSiteUser = siteUserRepository.findByEmailAndAuthType(userInfo.getEmail(), authType);
 
         if (optionalSiteUser.isPresent()) {
             SiteUser siteUser = optionalSiteUser.get();
             return getSignInResponse(siteUser);
         }
 
-        return getSignUpPrepareResponse(userInfoDto);
+        return getSignUpPrepareResponse(userInfo, authType);
     }
 
-    protected final OAuthSignInResponse getSignInResponse(SiteUser siteUser) {
+    private OAuthSignInResponse getSignInResponse(SiteUser siteUser) {
         SignInResponse signInResponse = signInService.signIn(siteUser);
         return new OAuthSignInResponse(true, signInResponse.accessToken(), signInResponse.refreshToken());
     }
 
-    protected final SignUpPrepareResponse getSignUpPrepareResponse(OAuthUserInfoDto userInfoDto) {
-        String signUpToken = OAuthSignUpTokenProvider.generateAndSaveSignUpToken(userInfoDto.getEmail(), getAuthType());
+    private SignUpPrepareResponse getSignUpPrepareResponse(OAuthUserInfoDto userInfoDto, AuthType authType) {
+        String signUpToken = OAuthSignUpTokenProvider.generateAndSaveSignUpToken(userInfoDto.getEmail(), authType);
         return SignUpPrepareResponse.of(userInfoDto, signUpToken);
     }
-
-    protected abstract OAuthUserInfoDto getOAuthUserInfo(String code);
-
-    protected abstract AuthType getAuthType();
 }
