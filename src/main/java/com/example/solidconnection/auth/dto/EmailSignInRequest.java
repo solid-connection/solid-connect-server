96a494fcffd16ff2d2b2ae0f466ab127aebf628d
Gyuhyeok99 <ghkdrbgur13@naver.com>
2025-08-10 19:15:52 +0900

    refactor: 이메일 검증 조건 추가
    - @NotBlank와 @Email을 함께 사용하도록

--- src/main/java/com/example/solidconnection/auth/dto/EmailSignInRequest.java
+++ src/main/java/com/example/solidconnection/auth/dto/EmailSignInRequest.java
@@ -1,11 +1,13 @@
 package com.example.solidconnection.auth.dto;
 
 import com.example.solidconnection.auth.dto.validation.Password;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record EmailSignInRequest(
 
         @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "유효한 이메일 주소를 입력해주세요.")
         String email,
 
         @Password
