1cd2390fddffa5ed9a9c9eae13e100d1abbc59c3
Gyuhyeok99 <ghkdrbgur13@naver.com>
2025-08-11 23:17:14 +0900

    refactor: @TransactionalEventListener로 변경

--- src/main/java/com/example/solidconnection/mentor/service/MentoringEventHandler.java
+++ src/main/java/com/example/solidconnection/mentor/service/MentoringEventHandler.java
@@ -3,11 +3,11 @@ package com.example.solidconnection.mentor.service;
 import com.example.solidconnection.chat.service.ChatService;
 import com.example.solidconnection.mentor.dto.MentoringApprovedEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -15,9 +15,9 @@ public class MentoringEventHandler {
 
     private final ChatService chatService;
 
-    @EventListener
     @Async
     @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener
     public void handleMentoringApproved(MentoringApprovedEvent event) {
         chatService.createMentoringChatRoom(event.mentoringId(), event.mentorId(), event.menteeId());
     }
