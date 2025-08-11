abe5ae5a172841c86712f096d2f29d287eeb8677
nayonsoso <panda0329@naver.com>
2025-08-12 02:53:30 +0900

    feat: 비밀번호 임시 저장소 구현

--- src/main/java/com/example/solidconnection/auth/service/EmailSignUpTokenProvider.java
+++ src/main/java/com/example/solidconnection/auth/service/EmailSignUpTokenProvider.java
@@ -1,40 +1,37 @@
 package com.example.solidconnection.auth.service;
 
-import com.example.solidconnection.auth.domain.TokenType;
 import com.example.solidconnection.auth.dto.EmailSignUpTokenRequest;
 import com.example.solidconnection.common.exception.CustomException;
 import com.example.solidconnection.common.exception.ErrorCode;
 import com.example.solidconnection.siteuser.domain.AuthType;
 import com.example.solidconnection.siteuser.repository.SiteUserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class EmailSignUpTokenProvider {
 
-    private final PasswordEncoder passwordEncoder;
-    private final TokenProvider tokenProvider;
     private final SignUpTokenProvider signUpTokenProvider;
     private final SiteUserRepository siteUserRepository;
+    private final PasswordTemporaryStorage passwordTemporaryStorage;
 
+    @Transactional(readOnly = true)
     public String issueEmailSignUpToken(EmailSignUpTokenRequest request) {
         String email = request.email();
+        String password = request.password();
+
         if (siteUserRepository.existsByEmailAndAuthType(email, AuthType.EMAIL)) {
             throw new CustomException(ErrorCode.USER_ALREADY_EXISTED);
         }
 
-        String signUpToken = signUpTokenProvider.generateAndSaveSignUpToken(email, AuthType.EMAIL);
-        String password = request.password();
-        String encodedPassword = passwordEncoder.encode(password);
-        // todo: 비밀번호 임시 저장 로직 추가
-
-        return tokenProvider.saveToken(signUpToken, TokenType.SIGN_UP);
+        passwordTemporaryStorage.save(email, password);
+        return signUpTokenProvider.generateAndSaveSignUpToken(email, AuthType.EMAIL);
     }
 
-    public String getTemporarySavedPassword(String signUpToken) {
-        // todo: 임시 저장된 비밀번호를 가져오는 로직 추가
-        return "";
+    public String getTemporarySavedPassword(String email) {
+        return passwordTemporaryStorage.findByEmail(email) // 임시 저장된 비밀번호가 없다면 signUpToken에 문제가 있는 것
+                .orElseThrow(() -> new CustomException(ErrorCode.SIGN_UP_TOKEN_INVALID));
     }
 }
---
+++ src/main/java/com/example/solidconnection/auth/service/PasswordTemporaryStorage.java
@@ -0,0 +1,41 @@
+package com.example.solidconnection.auth.service;
+
+import com.example.solidconnection.auth.domain.TokenType;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PasswordTemporaryStorage {
+
+    private static final String KEY_PREFIX = "password:";
+
+    private final RedisTemplate<String, String> redisTemplate;
+    private final PasswordEncoder passwordEncoder;
+
+    public void save(String email, String rawPassword) {
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        redisTemplate.opsForValue().set(
+                convertToKey(email),
+                encodedPassword,
+                TokenType.SIGN_UP.getExpireTime(),
+                TimeUnit.MILLISECONDS
+        );
+    }
+
+    public Optional<String> findByEmail(String email) {
+        String encodedPassword = redisTemplate.opsForValue().getAndDelete(convertToKey(email));
+        if (encodedPassword == null) {
+            return Optional.empty();
+        }
+        return Optional.of(encodedPassword);
+    }
+
+    private String convertToKey(String email) {
+        return KEY_PREFIX + email;
+    }
+}
