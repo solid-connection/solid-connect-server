1c18a5d2991c9570870e12762596649c72ad7209
seonghyeok <chosh2001@naver.com>
2025-08-14 22:54:05 +0900

    chore: 불필요한 Modifying 어노테이션 제거

--- src/main/java/com/example/solidconnection/location/country/repository/InterestedCountryRepository.java
+++ src/main/java/com/example/solidconnection/location/country/repository/InterestedCountryRepository.java
@@ -3,12 +3,10 @@ package com.example.solidconnection.location.country.repository;
 import com.example.solidconnection.location.country.domain.InterestedCountry;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 
 public interface InterestedCountryRepository extends JpaRepository<InterestedCountry, Long> {
 
     List<InterestedCountry> findAllBySiteUserId(long siteUserId);
 
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
     void deleteBySiteUserId(long siteUserId);
 }
--- src/main/java/com/example/solidconnection/location/region/repository/InterestedRegionRepository.java
+++ src/main/java/com/example/solidconnection/location/region/repository/InterestedRegionRepository.java
@@ -3,12 +3,10 @@ package com.example.solidconnection.location.region.repository;
 import com.example.solidconnection.location.region.domain.InterestedRegion;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 
 public interface InterestedRegionRepository extends JpaRepository<InterestedRegion, Long> {
 
     List<InterestedRegion> findAllBySiteUserId(long siteUserId);
 
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
     void deleteBySiteUserId(long siteUserId);
 }
