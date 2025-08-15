728e792a185e6d36add5bf542611e13d68edecbf
nayonsoso <panda0329@naver.com>
2025-08-15 11:39:26 +0900

    refactor: 리프레시 토큰 값이 비어있는 경우도 예외 처리

--- src/main/java/com/example/solidconnection/auth/controller/RefreshTokenCookieManager.java
+++ src/main/java/com/example/solidconnection/auth/controller/RefreshTokenCookieManager.java
@@ -52,16 +52,24 @@ public class RefreshTokenCookieManager {
     }
 
     public String getRefreshToken(HttpServletRequest request) {
+        // 쿠키가 없거나 비어있는 경우 예외 발생
         Cookie[] cookies = request.getCookies();
         if (cookies == null || cookies.length == 0) {
             throw new CustomException(REFRESH_TOKEN_NOT_EXISTS);
         }
 
-        return Arrays.stream(cookies)
+        // refreshToken 쿠키가 없는 경우 예외 발생
+        Cookie refreshTokenCookie = Arrays.stream(cookies)
                 .filter(cookie -> COOKIE_NAME.equals(cookie.getName()))
                 .findFirst()
-                .orElseThrow(() -> new CustomException(REFRESH_TOKEN_NOT_EXISTS))
-                .getValue();
+                .orElseThrow(() -> new CustomException(REFRESH_TOKEN_NOT_EXISTS));
+
+        // 쿠키 값이 비어있는 경우 예외 발생
+        String refreshToken = refreshTokenCookie.getValue();
+        if (refreshToken == null || refreshToken.isBlank()) {
+            throw new CustomException(REFRESH_TOKEN_NOT_EXISTS);
+        }
+        return refreshToken;
     }
 }
 
