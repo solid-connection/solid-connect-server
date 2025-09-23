ff6f22eb11084f96bcb3c3a572c3e87f2ff8ded4
seonghyeok cho <65901319+whqtker@users.noreply.github.com>
2025-09-23 12:56:10 +0900

    feat: 사용자 차단 관련 엔티티 및 DDL 작성 (#507)
    * feat: 사용자 차단 엔티티 작성
    
    * feat: 사용자 차단 테이블 DDL 작성

---
+++ src/main/java/com/example/solidconnection/siteuser/domain/UserBlock.java
@@ -0,0 +1,37 @@
+package com.example.solidconnection.siteuser.domain;
+
+import com.example.solidconnection.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@AllArgsConstructor
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uk_user_block_blocker_id_blocked_id",
+                columnNames = {"blocker_id", "blocked_id"}
+        )
+})
+public class UserBlock extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "blocker_id", nullable = false)
+    private long blockerId;
+
+    @Column(name = "blocked_id", nullable = false)
+    private long blockedId;
+}
