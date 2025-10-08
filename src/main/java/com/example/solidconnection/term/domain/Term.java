d0e15b4a9414b9068bdb4ec92db0a77473f82635
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-10-08 17:21:44 +0900

    feat: 학기 테이블 추가

---
+++ src/main/java/com/example/solidconnection/term/domain/Term.java
@@ -0,0 +1,49 @@
+package com.example.solidconnection.term.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@AllArgsConstructor
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uk_term_name",
+                columnNames = {"name"}
+        )
+})
+public class Term {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20)
+    private String name;
+
+    @Column(name = "is_current", nullable = false)
+    private boolean isCurrent = false;
+
+    public Term(String name, boolean isCurrent) {
+        this.name = name;
+        this.isCurrent = isCurrent;
+    }
+
+    public void setAsCurrent() {
+        this.isCurrent = true;
+    }
+
+    public void setAsNotCurrent() {
+        this.isCurrent = false;
+    }
+}
