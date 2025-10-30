16f46141926d83e1cad91953d7bd3bf39427e2ac
Gyuhyeok99 <ghkdrbgur13@naver.com>
2025-10-30 21:20:04 +0900

    test: 지원서 제출 시 지원 대학명 응답 관련 테스트 수정

--- src/test/java/com/example/solidconnection/application/service/ApplicationSubmissionServiceTest.java
+++ src/test/java/com/example/solidconnection/application/service/ApplicationSubmissionServiceTest.java
@@ -86,8 +86,16 @@ class ApplicationSubmissionServiceTest {
         // then
         Application savedApplication = applicationRepository.findBySiteUserIdAndTerm(user.getId(), term).orElseThrow();
         assertAll(
+                () -> assertThat(response.totalApplyCount())
+                        .isEqualTo(APPLICATION_UPDATE_COUNT_LIMIT),
                 () -> assertThat(response.applyCount())
                         .isEqualTo(savedApplication.getUpdateCount()),
+                () -> assertThat(response.appliedUniversities().firstChoiceUnivApplyInfo())
+                        .isEqualTo(괌대학_A_지원_정보.getKoreanName()),
+                () -> assertThat(response.appliedUniversities().secondChoiceUnivApplyInfo())
+                        .isEqualTo(괌대학_B_지원_정보.getKoreanName()),
+                () -> assertThat(response.appliedUniversities().thirdChoiceUnivApplyInfo())
+                        .isEqualTo(서던덴마크대학교_지원_정보.getKoreanName()),
                 () -> assertThat(savedApplication.getVerifyStatus())
                         .isEqualTo(VerifyStatus.APPROVED),
                 () -> assertThat(savedApplication.isDelete())
