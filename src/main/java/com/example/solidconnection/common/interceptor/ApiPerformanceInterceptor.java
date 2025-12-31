d265e3bf5ae77b6d6a9d57ef9541c3f27febee7b
sukangpunch <popora99@naver.com>
2026-01-01 03:18:39 +0900

    feat: API 수행시간 로깅 인터셉터 추가

---
+++ src/main/java/com/example/solidconnection/common/interceptor/ApiPerformanceInterceptor.java
@@ -0,0 +1,84 @@
+package com.example.solidconnection.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ApiPerformanceInterceptor implements HandlerInterceptor {
+    private static final String ROUTE_PATTERN_ATTRIBUTE = "routePattern";
+    private static final String START_TIME_ATTRIBUTE = "startTime";
+    private static final String REQUEST_URI_ATTRIBUTE = "requestUri";
+    private static final int RESPONSE_TIME_THRESHOLD = 3_000;
+    private static final Logger API_PERF = LoggerFactory.getLogger("API_PERF");
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler
+    ) throws Exception {
+
+        long startTime = System.currentTimeMillis();
+
+        request.setAttribute(START_TIME_ATTRIBUTE, startTime);
+        request.setAttribute(REQUEST_URI_ATTRIBUTE, request.getRequestURI());
+
+        Object bestMatchingPattern = request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+
+        if(bestMatchingPattern instanceof String s) {
+            request.setAttribute(ROUTE_PATTERN_ATTRIBUTE, s);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler,
+            Exception ex
+    ) throws Exception {
+        Long startTime = (Long) request.getAttribute(START_TIME_ATTRIBUTE);
+        if(startTime == null) {
+            return;
+        }
+
+        long responseTime = System.currentTimeMillis() - startTime;
+
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+        int status = response.getStatus();
+
+        if (responseTime > RESPONSE_TIME_THRESHOLD) {
+            API_PERF.warn(
+                    "type=API_Performance method_type={} uri={} response_time={} status={}",
+                    method, uri, responseTime, status
+            );
+
+            log.warn("[API Performance] {} {} - {}ms [Status: {}]",
+                     method, uri, responseTime, status
+            );
+
+            return;
+        }
+
+        API_PERF.info(
+                "type=API_Performance method_type={} uri={} response_time={} status={}",
+                method, uri, responseTime, status
+        );
+
+        log.info("[API Performance]: {} {} - {}ms [Status: {}]",
+                 method, uri, responseTime, status
+        );
+    }
+}
