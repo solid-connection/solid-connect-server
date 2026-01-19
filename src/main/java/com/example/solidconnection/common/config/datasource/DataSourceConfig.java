b33ff3b1fb7427b5285ebcbff16139dc7ff1216f
sukangpunch <popora99@naver.com>
2026-01-20 00:22:55 +0900

    fix: flyway 전용 DataSource를 사용하도록 수정

---
+++ src/main/java/com/example/solidconnection/common/config/datasource/DataSourceConfig.java
@@ -0,0 +1,57 @@
+package com.example.solidconnection.common.config.datasource;
+
+import com.example.solidconnection.common.listener.QueryMetricsListener;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@RequiredArgsConstructor
+@Configuration
+public class DataSourceConfig {
+
+    private final QueryMetricsListener queryMetricsListener;
+
+    @Bean
+    @Primary
+    public DataSource proxyDataSource(DataSourceProperties props) {
+        DataSource dataSource = props.initializeDataSourceBuilder().build();
+
+        return ProxyDataSourceBuilder
+                .create(dataSource)
+                .listener(queryMetricsListener)
+                .name("main")
+                .build();
+    }
+
+    // Flyway 전용 DataSource (Proxy 미적용)
+    @Bean
+    @FlywayDataSource
+    public DataSource flywayDataSource(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.flyway.user}") String username,
+            @Value("${spring.flyway.password}") String password,
+            @Value("${spring.datasource.driverClassName}") String driverClassName
+    ) {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setPoolName("FlywayPool");
+
+        dataSource.setMinimumIdle(0);
+        dataSource.setMaximumPoolSize(2);
+        dataSource.setConnectionTimeout(10000);
+        dataSource.setIdleTimeout(60000);       // 1분으로 단축
+        dataSource.setMaxLifetime(300000);      // 최대 5분
+
+        return dataSource;
+    }
+}
