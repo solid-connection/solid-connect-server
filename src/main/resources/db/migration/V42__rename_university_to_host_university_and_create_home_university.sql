4e21053d98b7196bb891e2175d2cf00f659d9f47
seonghyeok <chosh2001@naver.com>
2026-01-24 20:49:25 +0900

    feat: 파견 대학 테이블명 변경 및 협정 대학 테이블 추가하는 DDL 작성

---
+++ src/main/resources/db/migration/V42__rename_university_to_host_university_and_create_home_university.sql
@@ -0,0 +1,25 @@
+RENAME TABLE university TO host_university;
+
+ALTER TABLE university_info_for_apply
+    DROP FOREIGN KEY FKd0257hco6uy2utd1xccjh3fal;
+
+ALTER TABLE university_info_for_apply
+    ADD CONSTRAINT fk_university_info_for_apply_host_university
+        FOREIGN KEY (university_id) REFERENCES host_university (id) ON DELETE NO ACTION;
+
+CREATE TABLE IF NOT EXISTS home_university
+(
+    id   BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100)          NOT NULL,
+    CONSTRAINT `PRIMARY` PRIMARY KEY (id),
+    CONSTRAINT uk_home_university_name UNIQUE (name)
+);
+
+ALTER TABLE host_university
+    ADD COLUMN home_university_id BIGINT NULL;
+
+ALTER TABLE host_university
+    ADD CONSTRAINT fk_host_university_home_university
+        FOREIGN KEY (home_university_id) REFERENCES home_university (id) ON DELETE NO ACTION;
+
+CREATE INDEX fk_host_university_home_university_idx ON host_university (home_university_id);
