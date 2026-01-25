7482dcf61da9a3cb99dd3c280addbc7c611b4a1d
seonghyeok <chosh2001@naver.com>
2026-01-25 16:55:12 +0900

    refactor: home university에 대한 fetch join 추가

--- src/main/java/com/example/solidconnection/university/repository/HomeUniversityRepository.java
+++ src/main/java/com/example/solidconnection/university/repository/HomeUniversityRepository.java
@@ -2,9 +2,12 @@ package com.example.solidconnection.university.repository;
 
 import com.example.solidconnection.university.domain.HomeUniversity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HomeUniversityRepository extends JpaRepository<HomeUniversity, Long> {
 
     List<HomeUniversity> findAllByIdIn(List<Long> ids);
+
+    Optional<HomeUniversity> findByName(String name);
 }
