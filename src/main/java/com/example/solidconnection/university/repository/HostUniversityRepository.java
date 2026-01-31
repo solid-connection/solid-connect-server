1bedd3d7d1f21abe61dfe1e134d81dc8f2f9bcb8
seonghyeok <chosh2001@naver.com>
2026-01-31 16:28:15 +0900

    feat: 파견 대학 검색 관련 QueryDSL로 구현

--- src/main/java/com/example/solidconnection/university/repository/HostUniversityRepository.java
+++ src/main/java/com/example/solidconnection/university/repository/HostUniversityRepository.java
@@ -4,10 +4,11 @@ import static com.example.solidconnection.common.exception.ErrorCode.UNIVERSITY_
 
 import com.example.solidconnection.common.exception.CustomException;
 import com.example.solidconnection.university.domain.HostUniversity;
+import com.example.solidconnection.university.repository.custom.HostUniversityFilterRepository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HostUniversityRepository extends JpaRepository<HostUniversity, Long> {
+public interface HostUniversityRepository extends JpaRepository<HostUniversity, Long>, HostUniversityFilterRepository {
 
     default HostUniversity getHostUniversityById(Long id) {
         return findById(id)
