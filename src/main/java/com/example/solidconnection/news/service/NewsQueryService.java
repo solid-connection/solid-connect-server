470b34976e1ffada1122ed940f2297dac1778e46
seonghyeok <chosh2001@naver.com>
2026-02-17 22:24:34 +0900

    refactor: 뉴스 조회 API를 하나로 통합
    - 서비스 계층에서 siteUserId == null을 기준으로 분기하도록

--- src/main/java/com/example/solidconnection/news/service/NewsQueryService.java
+++ src/main/java/com/example/solidconnection/news/service/NewsQueryService.java
@@ -1,5 +1,6 @@
 package com.example.solidconnection.news.service;
 
+import com.example.solidconnection.news.domain.News;
 import com.example.solidconnection.news.dto.NewsListResponse;
 import com.example.solidconnection.news.dto.NewsResponse;
 import com.example.solidconnection.news.repository.NewsRepository;
@@ -15,35 +16,24 @@ public class NewsQueryService {
     private final NewsRepository newsRepository;
 
     @Transactional(readOnly = true)
-    public NewsListResponse findAllNews(Long siteUserId) {
-        // 로그인하지 않은 경우
+    public NewsListResponse findNews(Long siteUserId, Long authorId) {
         if (siteUserId == null) {
-            List<NewsResponse> newsResponseList = newsRepository.findAll().stream()
-                    .sorted((a, b) -> b.getUpdatedAt().compareTo(a.getUpdatedAt()))
+            List<NewsResponse> newsResponseList = findNewsEntities(authorId).stream()
                     .map(news -> NewsResponse.of(news, null))
                     .toList();
             return NewsListResponse.from(newsResponseList);
         }
 
-        // 로그인한 경우
-        List<NewsResponse> newsResponseList = newsRepository.findAllNewsWithLikeStatus(siteUserId);
+        List<NewsResponse> newsResponseList = (authorId == null)
+                ? newsRepository.findAllNewsWithLikeStatus(siteUserId)
+                : newsRepository.findNewsByAuthorIdWithLikeStatus(authorId, siteUserId);
         return NewsListResponse.from(newsResponseList);
     }
 
-    @Transactional(readOnly = true)
-    public NewsListResponse findNewsByAuthorId(Long siteUserId, long authorId) {
-        // 로그인하지 않은 경우
-        if (siteUserId == null) {
-            List<NewsResponse> newsResponseList = newsRepository.findAllBySiteUserIdOrderByUpdatedAtDesc(authorId)
-                    .stream()
-                    .map(news -> NewsResponse.of(news, null))
-                    .toList();
-            return NewsListResponse.from(newsResponseList);
+    private List<News> findNewsEntities(Long authorId) {
+        if (authorId == null) {
+            return newsRepository.findAllByOrderByUpdatedAtDesc();
         }
-
-        // 로그인한 경우
-        List<NewsResponse> newsResponseList = newsRepository.findNewsByAuthorIdWithLikeStatus(authorId, siteUserId);
-
-        return NewsListResponse.from(newsResponseList);
+        return newsRepository.findAllBySiteUserIdOrderByUpdatedAtDesc(authorId);
     }
 }
