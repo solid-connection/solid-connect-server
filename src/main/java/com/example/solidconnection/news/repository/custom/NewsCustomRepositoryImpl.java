7e7b5c9bd50a58842a0f9f1f3044491ef177d49c
seonghyeok cho <65901319+whqtker@users.noreply.github.com>
2026-02-18 13:37:16 +0900

    feat: 전체 뉴스를 조회하는 API 구현 (#674)
    * feat: 전체 news 조회 API 구현
    
    - 기존 API에 author-id를 선택적으로 받도록
    
    * test: 전체 news 조회 관련 테스트 코드 작성
    
    * refactor: 날짜 오름차순으로 news 조회하는 JPA 메서드 추가
    
    * refactor: 뉴스 조회 API를 하나로 통합
    
    - 서비스 계층에서 siteUserId == null을 기준으로 분기하도록
    
    * refactor: 컨트롤러 계층에서 분기문 제거
    
    - 분기를 서비스 계층에게 위임했음
    
    * test: 뉴스 조회 관련 테스트 코드 수정

--- src/main/java/com/example/solidconnection/news/repository/custom/NewsCustomRepositoryImpl.java
+++ src/main/java/com/example/solidconnection/news/repository/custom/NewsCustomRepositoryImpl.java
@@ -13,6 +13,28 @@ public class NewsCustomRepositoryImpl implements NewsCustomRepository {
     private final EntityManager entityManager;
 
     @Override
+    public List<NewsResponse> findAllNewsWithLikeStatus(Long siteUserId) {
+        String jpql = """
+                      SELECT new com.example.solidconnection.news.dto.NewsResponse(
+                          n.id,
+                          n.title,
+                          n.description,
+                          n.thumbnailUrl,
+                          n.url,
+                          CASE WHEN ln.id IS NOT NULL THEN true ELSE false END,
+                          n.updatedAt
+                      )
+                      FROM News n
+                      LEFT JOIN LikedNews ln ON n.id = ln.newsId AND ln.siteUserId = :siteUserId
+                      ORDER BY n.updatedAt DESC
+                      """;
+
+        return entityManager.createQuery(jpql, NewsResponse.class)
+                .setParameter("siteUserId", siteUserId)
+                .getResultList();
+    }
+
+    @Override
     public List<NewsResponse> findNewsByAuthorIdWithLikeStatus(long authorId, Long siteUserId) {
         String jpql = """
                       SELECT new com.example.solidconnection.news.dto.NewsResponse(
