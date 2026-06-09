011d40be88d4116de4d0b21ca4c8d8b2bbb72479
whqtker <chosh2001@inha.edu>
2026-06-09 15:42:49 +0900

    feat: DTO 및 검증 로직 변경

--- src/main/java/com/example/solidconnection/application/dto/UnivApplyInfoChoiceRequest.java
+++ src/main/java/com/example/solidconnection/application/dto/UnivApplyInfoChoiceRequest.java
@@ -2,17 +2,12 @@ package com.example.solidconnection.application.dto;
 
 import com.example.solidconnection.university.dto.validation.ValidUnivApplyInfoChoice;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 
 @ValidUnivApplyInfoChoice
 public record UnivApplyInfoChoiceRequest(
 
-        @JsonProperty("firstChoiceUniversityId")
-        Long firstChoiceUnivApplyInfoId,
-
-        @JsonProperty("secondChoiceUniversityId")
-        Long secondChoiceUnivApplyInfoId,
-
-        @JsonProperty("thirdChoiceUniversityId")
-        Long thirdChoiceUnivApplyInfoId) {
+        @JsonProperty("choices")
+        List<Long> univApplyInfoIds) {
 
 }
