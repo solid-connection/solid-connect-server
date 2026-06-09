7fc96b2537f73cd3fcb2049d8aa1f8db4e017e16
whqtker <chosh2001@inha.edu>
2026-06-09 15:55:23 +0900

    feat: 어드민 국내 대학 삽입 시 최대 지망 수 관련 DTO에 제약조건 추가

--- src/main/java/com/example/solidconnection/admin/university/dto/AdminHomeUniversityCreateRequest.java
+++ src/main/java/com/example/solidconnection/admin/university/dto/AdminHomeUniversityCreateRequest.java
@@ -1,12 +1,18 @@
 package com.example.solidconnection.admin.university.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record AdminHomeUniversityCreateRequest(
         @NotBlank(message = "협정 대학명은 필수입니다")
         @Size(max = 100, message = "협정 대학명은 100자 이하여야 합니다")
-        String name
+        String name,
+
+        @NotNull(message = "최대 지망 수는 필수입니다")
+        @Min(value = 1, message = "최대 지망 수는 1 이상이어야 합니다")
+        Integer maxChoiceCount
 ) {
 
 }
